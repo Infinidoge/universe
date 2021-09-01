b81e7ac2086d2a064307c725caeabf8ed6b490ea
Infinidoge <infinidoge@inx.moe>
2021-09-01 10:33:37 -0400

    style: run nixpkgs-fmt

--- hosts/bootstrap.nix
+++ hosts/bootstrap.nix
@@ -1,5 +1,4 @@
-{ profiles, ... }:
-{
+{ profiles, ... }: {
   # build with: `bud build bootstrap bootstrapIso`
   # reachable on the local link via ssh root@fe80::47%eno1
   # where 'eno1' is replaced by your own machine's network
