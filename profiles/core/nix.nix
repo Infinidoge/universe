9d1e1ac5efe986f9fa790e039dc359d6aa88ff41
Infinidoge <infinidoge@inx.moe>
2021-09-08 09:39:02 -0400

    fix: missing values in profiles/core/nix

--- profiles/core/nix.nix
+++ profiles/core/nix.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ config, pkgs, self, lib, ... }: {
 
   nix = {
     systemFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
@@ -26,24 +26,27 @@
   environment = {
     systemPackages = with pkgs; [ nix-index nixfmt nixpkgs-fmt ];
 
-    shellAliases = {
-      # nix
-      n = "nix";
-      np = "n profile";
-      ni = "np install";
-      nr = "np remove";
-      ns = "n search --no-update-lock-file";
-      nf = "n flake";
-      nepl = "n repl '<nixpkgs>'";
-      srch = "ns nixos";
-      orch = "ns override";
-      nrb = ifSudo "sudo nixos-rebuild";
-      mn = ''
-        manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | sk --preview="manix '{}'" | xargs manix
-      '';
+    shellAliases =
+      let ifSudo = lib.mkIf config.security.sudo.enable;
+      in
+      {
+        # nix
+        n = "nix";
+        np = "n profile";
+        ni = "np install";
+        nr = "np remove";
+        ns = "n search --no-update-lock-file";
+        nf = "n flake";
+        nepl = "n repl '<nixpkgs>'";
+        srch = "ns nixos";
+        orch = "ns override";
+        nrb = ifSudo "sudo nixos-rebuild";
+        mn = ''
+          manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | sk --preview="manix '{}'" | xargs manix
+        '';
 
-      # fix nixos-option
-      nixos-option = "nixos-option -I nixpkgs=${self}/lib/compat";
-    };
+        # fix nixos-option
+        nixos-option = "nixos-option -I nixpkgs=${self}/lib/compat";
+      };
   };
 }
