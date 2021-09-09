e009b97ed1d83049c6711748f1685819e8280203
Infinidoge <infinidoge@inx.moe>
2021-09-08 20:47:21 -0400

    feat: add zsh, kitty, and starship to user

--- users/infinidoge/default.nix
+++ users/infinidoge/default.nix
@@ -1,7 +1,70 @@
 { config, self, lib, pkgs, ... }: {
   home-manager.users.infinidoge = { suites, ... }: {
     imports = suites.base;
+
+    programs.starship = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+
+      settings = { };
+    };
+
+    programs.kitty = {
+      enable = true;
+      font.package = (pkgs.nerdfonts.override { fonts = [ "DejaVuSansMono" ]; });
+      font.name = "DejaVuSansMono";
+      font.size = 16;
+    };
+
+    programs.zsh = {
+      enable = true;
+
+      enableCompletion = true;
+      # enableSyntaxHighlighting = true;
+      enableVteIntegration = true;
+
+      dotDir = ".config/zsh";
+
+      history.path = "$ZDOTDIR/.zsh_history";
+
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          # Display
+          "colorize"
+          "colored-man-pages"
+
+          # zsh modifications
+          "zsh-interactive-cd"
+          "zsh_reload"
+          "command-not-found"
+
+          # Aliases
+          "alias-finder"
+
+          # Applications
+          ## Docker
+          "docker"
+          ## Python
+          "pip"
+          "python"
+          ## Systemd
+          "systemd"
+          ## Git
+          "git"
+          "github"
+          "gitignore"
+          ## Emacs
+          "emacs"
+          ## Vim
+          "fancy-ctrl-z"
+        ];
+      };
+    };
   };
+
+  environment.pathsToLink = [ "/share/zsh" ];
 
   users.users.infinidoge = {
     uid = 1000;
@@ -10,5 +73,6 @@
     description = "Infinidoge";
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+    shell = pkgs.zsh;
   };
 }
