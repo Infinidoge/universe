6f680df9c05183b3c4d889df68dc8853755e60ac
Infinidoge <infinidoge@inx.moe>
2021-09-10 10:52:25 -0400

    feat: flesh out Infini-LAPTOP

--- hosts/Infini-LAPTOP/default.nix
+++ hosts/Infini-LAPTOP/default.nix
@@ -1,1 +1,18 @@
-{ ... }: { }
+{ suites, profiles, pkgs, ... }: {
+  imports = suites.graphic
+    ++ [ ./hardware-configuration.nix ]
+    ++ (with profiles;
+    [
+      boot.systemd-boot
+
+      networking.wireless
+      hardware.sound
+      hardware.amdgpu
+      # peripherals.printing
+    ]
+  );
+
+  system.stateVersion = "21.11";
+
+
+}
