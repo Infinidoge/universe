cc42771ee4f18e2ec77bee6660f8a3b6afe38694
Infinidoge <infinidoge@inx.moe>
2021-09-16 00:23:49 -0400

    fix: password-store settings need to be strings

--- users/profiles/pass.nix
+++ users/profiles/pass.nix
@@ -5,8 +5,8 @@
     settings = {
       PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
       PASSWORD_STORE_KEY = "2C0BB53DFA3BB79F64B0ADCAAA0E1F926FAF03C0"; # Named "Infinidoge (Master Password)"
-      PASSWORD_STORE_GENERATED_LENGTH = 20;
-      PASSWORD_STORE_CLIP_TIME = 60;
+      PASSWORD_STORE_GENERATED_LENGTH = "20";
+      PASSWORD_STORE_CLIP_TIME = "60";
     };
   };
 
