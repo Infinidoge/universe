bc927f134b57007b0203aa5979f92684bc0badaf
Infinidoge <infinidoge@inx.moe>
2021-09-16 12:17:04 -0400

    feat: add disks to Infini-SWIFT config

--- hosts/Infini-SWIFT/hardware-configuration.nix
+++ hosts/Infini-SWIFT/hardware-configuration.nix
@@ -15,4 +15,17 @@
   boot.extraModulePackages = [ ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+
+  fileSystems."/" = { device = "/dev/disk/by-label/4a964f22-e270-42df-ae54-00f91d89edd9";
+                      fsType = "ext4";
+                    };
+  fileSystems."/boot" = { device = "/dev/disk/by-uuid/6836-6848";
+                          fsType = "vfat";
+                        };
+
+  swapDevices = [
+    {
+      device = "/dev/disk/by-uuid/8d0f13e7-135e-4dfd-9090-fabf1fc406c2";
+    }
+  ];
 }
