9ff5187cf83cc8f647162535ef7273c0e0a20134
Infinidoge <infinidoge@inx.moe>
2021-09-19 17:37:44 -0400

    fix: add fonts used by Qtile config

--- profiles/graphical/qtile.nix
+++ profiles/graphical/qtile.nix
@@ -4,4 +4,6 @@
   services.xserver.windowManager.qtile.enable = true;
 
   environment.systemPackages = with pkgs; [ xsecurelock ];
+
+  fonts.fonts = with pkgs; [ powerline-fonts ubuntu_font_family ];
 }
