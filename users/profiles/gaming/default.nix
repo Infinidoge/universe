773357e6224e2ea1b604650ba5ab0d6313936f5f
Infinidoge <infinidoge@inx.moe>
2021-09-20 00:31:22 -0400

    fix: remove broken wine install

--- users/profiles/gaming/default.nix
+++ users/profiles/gaming/default.nix
@@ -1,10 +1,6 @@
 { pkgs, ... }: {
   imports = [ ./steam.nix ];
 
-  environment.systemPackages = with pkgs; [
-    wineWowPackages.stable
-  ];
-
   home.packages = with pkgs; [
     wineWowPackages.stable
     (multimc.override { msaClientID = "01524508-0110-46fc-b468-362d31ca41e6"; })
