21bd77098fa3edaf50d0bcaaac5420f02c38efad
Infinidoge <infinidoge@inx.moe>
2021-09-20 21:45:06 -0400

    fix: add parenthesis to flattenListSet

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,6 @@
 { lib }:
 lib.makeExtensible (self:
   {
-    flattenListSet = imports: lib.lists.flatten builtins.concatLists builtins.attrValues imports;
+    flattenListSet = imports: (lib.lists.flatten (builtins.concatLists (builtins.attrValues imports)));
   }
 )
