0e56b0e14a5e350b137b320d78dda74b3f6a63f3
Infinidoge <infinidoge@inx.moe>
2021-09-26 16:38:15 -0400

    feat: clean up doom emacs configuration

--- users/profiles/emacs/default.nix
+++ users/profiles/emacs/default.nix
@@ -1,9 +1,33 @@
 { config, pkgs, ... }: {
-  services.emacs = {
-    enable = true;
-  };
+  services.emacs.enable = true;
 
-  home.sessionPath = [
-    "${config.xdg.configHome}/emacs/bin"
-  ];
+  home = {
+    sessionPath = [
+      "${config.xdg.configHome}/emacs/bin"
+    ];
+
+    sessionVariables = {
+      DOOMDIR = "/etc/nixos/users/profiles/emacs/doom/";
+    };
+
+    packages = with pkgs; [
+      emacs
+      ripgrep
+      coreutils
+      cmake
+      fd
+      fzf
+      clang
+      mu
+      isync
+      tetex
+      jq
+      gnumake
+      shellcheck
+      nodejs
+      nodePackages.prettier
+
+      (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
+    ];
+  };
 }
