ed97f22ac02c48e8d5724ed6607ff9cf4bd0c237
Infinidoge <infinidoge@inx.moe>
2021-10-01 18:34:44 -0400

    feat: add flattenSetList
    Using the function reduces continual use of lib.lists.flatten, by
    applying it to each of the attributes of a set, assuming they contain lists.

--- lib/default.nix
+++ lib/default.nix
@@ -2,5 +2,6 @@
 lib.makeExtensible (self:
   {
     flattenListSet = imports: (lib.lists.flatten (builtins.concatLists (builtins.attrValues imports)));
+    flattenSetList = attrSet: (builtins.mapAttrs (name: value: lib.lists.flatten value) attrSet);
   }
 )
