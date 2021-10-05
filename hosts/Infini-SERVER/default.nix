71dcec4cbabd6c4a956835a53c795f95607884dc
Infinidoge <infinidoge@inx.moe>
2021-10-05 08:47:07 -0400

    feat: add initial server configuration

---
+++ hosts/Infini-SERVER/default.nix
@@ -0,0 +1,11 @@
+{ suites, profiles, pkgs, lib, ... }: {
+  imports = lib.our.flattenListSet {
+    suites = with suites; [ base ];
+    imports = [ ];
+    profiles = with profiles; [
+      boot.grub
+
+      hardware.gpu.nvidia
+    ];
+  };
+}
