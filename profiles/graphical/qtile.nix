5077590e0f8193412553920ab217a4ffdf73afa2
Infinidoge <infinidoge@inx.moe>
2021-10-06 14:12:52 -0400

    feat: add rofi package to qtile

--- profiles/graphical/qtile.nix
+++ profiles/graphical/qtile.nix
@@ -3,7 +3,10 @@
 
   services.xserver.windowManager.qtile.enable = true;
 
-  environment.systemPackages = with pkgs; [ xsecurelock ];
+  environment.systemPackages = with pkgs; [
+    xsecurelock
+    rofi
+  ];
 
   fonts.fonts = with pkgs; [ powerline-fonts ubuntu_font_family ];
 }
