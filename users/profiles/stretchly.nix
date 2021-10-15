388e8c0298bc3d1aaba121ba497a58cc91933fcc
Infinidoge <infinidoge@inx.moe>
2021-10-15 10:20:33 -0400

    move stretchly config from user to profile

--- users/profiles/stretchly.nix
+++ users/profiles/stretchly.nix
@@ -1,3 +1,9 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ stretchly ];
+{ config, pkgs, ... }: {
+  home = {
+    packages = with pkgs; [ stretchly ];
+    file.stretchly_config = {
+      source = ./../infinidoge/config/stretchly.json;
+      target = "${config.xdg.configHome}/Stretchly/config.json";
+    };
+  };
 }
