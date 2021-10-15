36d4a4823816d33d03c1cad600dda531bcedacb2
Infinidoge <infinidoge@inx.moe>
2021-10-15 09:47:10 -0400

    added base suite to root user

--- users/root/default.nix
+++ users/root/default.nix
@@ -1,5 +1,12 @@
-{ ... }:
-{
+{ lib, ... }: {
   users.users.root.hashedPassword =
     "PASSWORD SET IN THE FUTURE";
+
+  home-manager.users.root = { suites, profiles, ... }: {
+    imports = lib.lists.flatten [
+      (with suites; [
+        base
+      ])
+    ];
+  };
 }
