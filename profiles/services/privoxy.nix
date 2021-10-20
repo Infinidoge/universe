29bbc8e893f1028b943f328e75e3ceb1ae56f3fd
Infinidoge <infinidoge@inx.moe>
2021-10-20 13:40:55 -0400

    services.privoxy: add ssh-tunnel

--- profiles/services/privoxy.nix
+++ profiles/services/privoxy.nix
@@ -1,10 +1,20 @@
-{ ... }: {
-  services.privoxy = {
-    enable = true;
+{ pkgs, ... }: {
+  services = {
+    privoxy = {
+      enable = true;
 
-    settings = {
-      enable-edit-actions = true;
-      forward-socks5 = "/ 127.0.0.1:1337 .";
+      settings = {
+        enable-edit-actions = true;
+        forward-socks5 = "/ 127.0.0.1:1337 .";
+        listen-address = "localhost:8118";
+      };
+    };
+
+    ssh-tunnel = {
+      enable = true;
+      server = "infinidoge@server.doge-inc.net -p 245 -i /home/infinidoge/.ssh/id_ed25519";
+      requiredBy = [ "privoxy.service" ];
+      forwards.dynamic = [ 1337 ];
     };
   };
 }
