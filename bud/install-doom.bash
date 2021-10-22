724f2a8bb70d2480d16f17eed6d279b0c668290d
Infinidoge <infinidoge@inx.moe>
2021-10-22 13:00:57 -0400

    fix(bud/install-doom): change -x to ! -d

--- bud/install-doom.bash
+++ bud/install-doom.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -x $XDG_CONFIG_HOME/emacs ]; then
+if [ ! -d $XDG_CONFIG_HOME/emacs ]; then
   git clone --depth 1 https://github.com/hlissner/doom-emacs $XDG_CONFIG_HOME/emacs
   $XDG_CONFIG_HOME/emacs/bin/doom install "$@"
 else
