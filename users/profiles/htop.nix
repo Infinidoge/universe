72e7f0b5e67c7880033eac33e56f9871d879db60
Infinidoge <infinidoge@inx.moe>
2021-10-25 00:27:23 -0400

    users/profiles: add htop profile

---
+++ users/profiles/htop.nix
@@ -0,0 +1,39 @@
+{ config, ... }: {
+  programs.htop = {
+    enable = true;
+    settings = {
+      color_scheme = 0;
+      cpu_count_from_one = 0;
+      delay = 15;
+      fields = with config.lib.htop.fields; [
+        PID
+        USER
+        PRIORITY
+        NICE
+        M_SIZE
+        M_RESIDENT
+        M_SHARE
+        STATE
+        PERCENT_CPU
+        PERCENT_MEM
+        TIME
+        CWD
+        COMM
+      ];
+      highlight_base_name = 1;
+      highlight_megabytes = 1;
+      highlight_threads = 1;
+    } // (with config.lib.htop; leftMeters [
+      (bar "AllCPUs2")
+      (bar "Memory")
+      (bar "Swap")
+      (text "Zram")
+    ]) // (with config.lib.htop; rightMeters [
+      (text "Tasks")
+      (text "LoadAverage")
+      (text "Uptime")
+      (text "Systemd")
+    ]);
+
+  };
+}
