6642d8839634fcdff4e355fcf9441d6989fb46ef
Infinidoge <infinidoge@inx.moe>
2021-10-25 09:46:16 -0400

    refactor(profiles/stretchly): use xdg.configFile

--- users/profiles/stretchly.nix
+++ users/profiles/stretchly.nix
@@ -1,9 +1,6 @@
 { config, pkgs, ... }: {
-  home = {
-    packages = with pkgs; [ stretchly ];
-    file.stretchly_config = {
-      source = ./../infinidoge/config/stretchly.json;
-      target = "${config.xdg.configHome}/Stretchly/config.json";
-    };
-  };
+  home.packages = with pkgs; [ stretchly ];
+
+  xdg.configFile."Stretchly/config.json".source =
+    ./../infinidoge/config/stretchly.json;
 }
