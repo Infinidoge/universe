dd68d6dc7c402160847dfbab838cb0ac99113726
Infinidoge <infinidoge@inx.moe>
2021-10-29 09:11:10 -0400

    profiles/discord: remote better-friends-list

--- flake.nix
+++ flake.nix
@@ -54,7 +54,6 @@
     powercord-overlay.url = "github:LavaDesu/powercord-overlay";
     powercord-overlay.inputs.nixpkgs.follows = "latest";
 
-    discord-Better-Friends-List = { url = "github:E-boi/Better-Friends-List"; flake = false; };
     discord-Custom-Volume-Range = { url = "github:PandaDriver156/Custom-Volume-Range"; flake = false; };
     discord-In-app-notifs = { url = "github:BenSegal855/In-app-notifs"; flake = false; };
     discord-NSFW-tags = { url = "github:E-boi/NSFW-tags"; flake = false; };
--- users/profiles/discord.nix
+++ users/profiles/discord.nix
@@ -2,7 +2,6 @@
   home.packages = with pkgs; [
     (discord-plugged.override {
       plugins = with inputs; [
-        discord-Better-Friends-List
         discord-Custom-Volume-Range
         discord-In-app-notifs
         discord-NSFW-tags
