97f1f6fd15130fbd5da5e2076312913e42a5be67
Infinidoge <infinidoge@inx.moe>
2021-11-01 09:31:33 -0400

    hardware/gpu/intel: ensure intel driver is loaded

--- profiles/hardware/gpu/intel.nix
+++ profiles/hardware/gpu/intel.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
   imports = [ ./common.nix ];
 
+  services.xserver.videoDrivers = [ "intel" ];
+
   hardware.opengl.extraPackages = with pkgs; [
     intel-media-driver
     vaapiIntel
