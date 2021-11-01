506043dc416df8a8b7c671638cb8f1095053db15
Infinidoge <infinidoge@inx.moe>
2021-11-01 12:28:26 -0400

    gpu/intel(fix): don't specify 'intel' videoDriver
    This breaks things loading properly

--- profiles/hardware/gpu/intel.nix
+++ profiles/hardware/gpu/intel.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }: {
   imports = [ ./common.nix ];
 
-  services.xserver.videoDrivers = [ "intel" ];
-
   hardware.opengl.extraPackages = with pkgs; [
     intel-media-driver
     vaapiIntel
