affeec6e6dd484539544ad5f98150c8a67dde370
Infinidoge <infinidoge@inx.moe>
2021-11-04 00:31:51 -0400

    hosts/Infini-FRAMEWORK: slight refactor

--- hosts/Infini-FRAMEWORK/hardware-configuration.nix
+++ hosts/Infini-FRAMEWORK/hardware-configuration.nix
@@ -3,6 +3,9 @@
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, ... }:
 
+let
+  uuid = uuid: "/dev/disk/by-uuid/${uuid}";
+in
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
@@ -13,46 +16,42 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems =
-    let
-      uuid = uuid: "/dev/disk/by-uuid/${uuid}";
-    in
-    {
-      "/" = {
-        device = "none";
-        fsType = "tmpfs";
-        options = [ "defaults" "size=4G" "mode=755" ];
-      };
-
-      "/persist" = {
-        device = uuid "a44af0ff-5667-465d-b80a-1934d1aab8d9";
-        fsType = "btrfs";
-        options = [ "subvol=root" "autodefrag" "noatime" ];
-        neededForBoot = true;
-      };
-
-      "/nix" = {
-        device = uuid "a44af0ff-5667-465d-b80a-1934d1aab8d9";
-        fsType = "btrfs";
-        options = [ "subvol=nix" "autodefrag" "noatime" ];
-        neededForBoot = true;
-      };
-
-      "/boot" = {
-        device = uuid "a44af0ff-5667-465d-b80a-1934d1aab8d9";
-        fsType = "btrfs";
-        options = [ "subvol=boot" "autodefrag" "noatime" ];
-        neededForBoot = true;
-      };
-
-      "/boot/efi" = {
-        device = uuid "3FC9-0182";
-        fsType = "vfat";
-        neededForBoot = true;
-      };
+  fileSystems = {
+    "/" = {
+      device = "none";
+      fsType = "tmpfs";
+      options = [ "defaults" "size=4G" "mode=755" ];
     };
 
-  swapDevices = [{ device = "/dev/disk/by-uuid/28672ffb-9f1c-462b-b49d-8a14b3dd72b3"; }];
+    "/persist" = {
+      device = uuid "a44af0ff-5667-465d-b80a-1934d1aab8d9";
+      fsType = "btrfs";
+      options = [ "subvol=root" "autodefrag" "noatime" ];
+      neededForBoot = true;
+    };
+
+    "/nix" = {
+      device = uuid "a44af0ff-5667-465d-b80a-1934d1aab8d9";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "autodefrag" "noatime" ];
+      neededForBoot = true;
+    };
+
+    "/boot" = {
+      device = uuid "a44af0ff-5667-465d-b80a-1934d1aab8d9";
+      fsType = "btrfs";
+      options = [ "subvol=boot" "autodefrag" "noatime" ];
+      neededForBoot = true;
+    };
+
+    "/boot/efi" = {
+      device = uuid "3FC9-0182";
+      fsType = "vfat";
+      neededForBoot = true;
+    };
+  };
+
+  swapDevices = [{ device = uuid "28672ffb-9f1c-462b-b49d-8a14b3dd72b3"; }];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   # high-resolution display
