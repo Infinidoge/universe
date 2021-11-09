9c11766c7dcc4a622ba08d3353ed6e379332a2fa
Infinidoge <infinidoge@inx.moe>
2021-11-08 20:21:31 -0500

    secrets: start secrets.nix revamp

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,22 @@
+{ lib, ... }:
+with lib;
 let
   # set ssh public keys here for your system and user
-  system = "";
-  user = "";
-  allKeys = [ system user ];
+  systems = {
+    Infini-DESKTOP = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9a6DiYuEeA+bX4rWgWGRUZwtln8sXtxCG9fPuvp9Hx root@Infini-DESKTOP";
+    Infini-FRAMEWORK = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF7PmPq/7e+YIVAvIcs6EOJ3pZVJhinwus6ZauJ3aVp0 root@Infini-FRAMEWORK";
+    # Infini-RPI = "";
+    # Infini-SERVER = "";
+    # Infini-STICK = "";
+    # Infini-SWIFT = "";
+  };
+  users = {
+    infinidoge = "";
+  };
+  allKeys = flatten [
+    (attrValues systems)
+    (attrValues users)
+  ];
 in
 {
   "secret.age".publicKeys = allKeys;
