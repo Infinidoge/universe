fec66eeb42b2fbe63538555d854d344cd9f7be0f
Infinidoge <infinidoge@inx.moe>
2021-11-10 11:50:39 -0500

    modules/bindmounts: add module for home bind mounts

--- lib/default.nix
+++ lib/default.nix
@@ -1,7 +1,33 @@
 { lib }:
 lib.makeExtensible (self:
-  {
-    flattenListSet = imports: (lib.lists.flatten (builtins.concatLists (builtins.attrValues imports)));
-    flattenSetList = attrSet: (builtins.mapAttrs (name: value: lib.lists.flatten value) attrSet);
+
+  with lib;
+  rec {
+    flattenListSet = imports: (flatten (concatLists (attrValues imports)));
+    flattenSetList = attrSet: (mapAttrs (name: value: flatten value) attrSet);
+
+
+    # ["/home/user/" "/.screenrc"] -> ["home" "user" ".screenrc"]
+    splitPath = paths:
+      (filter
+        (s: builtins.typeOf s == "string" && s != "")
+        (concatMap (builtins.split "/") paths)
+      );
+
+    # ["home" "user" ".screenrc"] -> "home/user/.screenrc"
+    dirListToPath = dirList: (concatStringsSep "/" dirList);
+
+    # ["/home/user/" "/.screenrc"] -> "/home/user/.screenrc"
+    concatPaths = paths:
+      let
+        prefix = optionalString (hasPrefix "/" (head paths)) "/";
+        path = dirListToPath (splitPath paths);
+      in
+      prefix + path;
+
+    sanitizeName = name:
+      replaceStrings
+        [ "." ] [ "" ]
+        (strings.sanitizeDerivationName (removePrefix "/" name));
   }
 )
