326a2b45307b4be33c143fd23c726f67342165ce
Infinidoge <infinidoge@inx.moe>
2021-11-11 16:22:30 -0500

    shells/nushell: fix starship prompt

--- users/profiles/shells/nushell.nix
+++ users/profiles/shells/nushell.nix
@@ -4,12 +4,7 @@
     settings = {
       complete_from_path = true;
       ctrlc_exit = true;
-      startup = [
-        "mkdir ~/.cache/starship"
-        "starship init nu | save ~/.cache/starship/init.nu"
-        "source ~/.cache/starship/init.nu"
-      ];
-      prompt = "starship_prompt";
+      prompt = "STARSHIP_SHELL= starship prompt";
     };
   };
 }
