7865441c268e2e8ee0c4004a4b639bf284490fa6
Infinidoge <infinidoge@inx.moe>
2021-11-11 00:24:51 -0500

    profiles/htop: update htop configuration

--- users/profiles/htop.nix
+++ users/profiles/htop.nix
@@ -2,9 +2,6 @@
   programs.htop = {
     enable = true;
     settings = {
-      color_scheme = 0;
-      cpu_count_from_one = 0;
-      delay = 15;
       fields = with config.lib.htop.fields; [
         PID
         USER
@@ -19,19 +16,32 @@
         TIME
         COMM
       ];
+      color_scheme = 0;
+      cpu_count_from_one = 0;
+      delay = 15;
       highlight_base_name = 1;
       highlight_megabytes = 1;
       highlight_threads = 1;
+      find_comm_in_cmdline = 1;
+      tree_view = 1;
+      header_margin = 1;
+      show_cpu_usage = 1;
+      show_cpu_frequency = 1;
+      show_cpu_temperature = 1;
+      update_process_names = 1;
     } // (with config.lib.htop; leftMeters [
-      (bar "AllCPUs2")
+      (bar "AllCPUs")
+      (bar "CPU")
+    ]) // (with config.lib.htop; rightMeters [
       (bar "Memory")
       (bar "Swap")
-      (text "Zram")
-    ]) // (with config.lib.htop; rightMeters [
+      (text "Blank")
       (text "Tasks")
       (text "LoadAverage")
       (text "Uptime")
       (text "Systemd")
+      (text "DiskIO")
+      (text "NetworkIO")
     ]);
 
   };
