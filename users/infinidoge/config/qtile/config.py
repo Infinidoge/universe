8bd765538154d5737185d32cb95e3ce053520635
Infinidoge <infinidoge@inx.moe>
2021-11-17 11:07:37 -0500

    config/qtile: fix pinentry not floating

--- users/infinidoge/config/qtile/config.py
+++ users/infinidoge/config/qtile/config.py
@@ -653,6 +653,7 @@ floating_layout = layout.Floating(
         Match(wm_class="ssh-askpass"),  # ssh-askpass
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
+        Match(wm_class="pinentry"),
     ]
 )
 
