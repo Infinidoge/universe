7822d542c9fa48b4abfb1fb9c0df24b072e0791c
Infinidoge <infinidoge@inx.moe>
2021-11-17 09:03:27 -0500

    profiles/shells: add kitty completion to bash/fish

--- users/profiles/shells/fish.nix
+++ users/profiles/shells/fish.nix
@@ -6,6 +6,9 @@
       enable = true;
       functions = { };
       shellAbbrs = { };
+      interactiveShellInit = ''
+        kitty + complete setup fish | source
+      '';
     };
 
     starship.enableFishIntegration = lib.mkIf config.programs.starship.enable true;
