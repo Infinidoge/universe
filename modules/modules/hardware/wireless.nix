77ac64e96b1c1f6ae32b24f6dc206b68ea60a1b7
Infinidoge <infinidoge@inx.moe>
2021-11-21 13:43:24 -0500

    hardware/wireless: add wireless, refactor form

---
+++ modules/modules/hardware/wireless.nix
@@ -0,0 +1,35 @@
+{ config, lib, ... }:
+with lib;
+with lib.hlissner;
+let
+  cfg = config.modules.hardware.wireless;
+in
+{
+  options.modules.hardware.wireless = {
+    enable = mkBoolOpt false;
+    bluetooth = {
+      enable = mkBoolOpt false;
+      blueman.enable = mkBoolOpt true;
+    };
+    wifi.enable = mkBoolOpt false;
+  };
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      modules.hardware.wireless = {
+        bluetooth.enable = true;
+        wifi.enable = true;
+      };
+    })
+    (mkIf cfg.bluetooth.enable {
+      hardware.bluetooth.enable = true;
+      services.blueman.enable = cfg.bluetooth.blueman.enable;
+    })
+    (mkIf cfg.wifi.enable {
+      networking.wireless = {
+        enable = true;
+        userControlled.enable = true;
+      };
+    })
+  ];
+}
