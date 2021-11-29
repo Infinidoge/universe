d07b733b96f3a47c0ccdf52a1281808961fc4354
Infinidoge <infinidoge@inx.moe>
2021-11-29 14:11:21 -0500

    hosts/Infini-SWIFT: update to modules system

--- hosts/Infini-SWIFT/default.nix
+++ hosts/Infini-SWIFT/default.nix
@@ -4,17 +4,9 @@
     imports = [ ./hardware-configuration.nix ];
     profiles = with profiles;
       [
-        boot.grub
-
         networking.wireless
 
-        (with hardware; [
-          gpu.amdgpu
-          wireless
-        ])
-
         btrfs
-        # peripherals.printing
 
         # services.privoxy
       ];
@@ -25,33 +17,15 @@
   powerManagement.resumeCommands = "${pkgs.kmod}/bin/rmod atkbd; ${pkgs.kmod}/bin/modprobe atkbd reset=1";
 
   modules = {
+    boot = {
+      grub.enable = true;
+    };
     hardware = {
       form.laptop = true;
+      gpu.amdgpu = true;
+      wireless.enable = true;
     };
   };
 
   networking.interfaces.wlan0.useDHCP = true;
-
-
-  # services.minecraft-servers = {
-  #   enable = true;
-  #   openFirewall = true;
-
-  #   servers = {
-  #     test = {
-  #       enable = true;
-  #       eula = true;
-  #       # declarative = true;
-  #       # serverProperties.server-port = 25565;
-  #     };
-
-  #     # test2 = {
-  #     #   enable = true;
-  #     #   eula = true;
-  #     #   declarative = true;
-  #     #   serverProperties.server-port = 25566;
-  #     # };
-  #   };
-  # };
-
 }
