e3ab8cf7e11a35588b084bb1fffd18d51df2831a
Infinidoge <infinidoge@inx.moe>
2021-11-30 13:16:52 -0500

    bud: remove get command

--- bud/default.nix
+++ bud/default.nix
@@ -1,12 +1,5 @@
 { pkgs, lib, budUtils, ... }: {
   bud.cmds = with pkgs; {
-    get = {
-      writer = budUtils.writeBashWithPaths [ nixUnstable git coreutils ];
-      synopsis = "get [DEST]";
-      help = "Copy the desired template to DEST";
-      script = ./get.bash;
-    };
-
     buildthis = {
       writer = budUtils.writeBashWithPaths [ nixUnstable nixos-rebuild git coreutils mercurial ];
       synopsis = "buildthis (switch|boot|test)";
