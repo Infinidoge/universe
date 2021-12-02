c9867532be13d993aeccf45883c0d7a248f4db08
Infinidoge <infinidoge@inx.moe>
2021-12-02 09:24:30 -0500

    peripherals/printing: add printing module

---
+++ modules/modules/hardware/peripherals/printing.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+with lib;
+with lib.hlissner;
+let
+  cfg = config.modules.hardware.peripherals.printing;
+in
+{
+  options.modules.hardware.peripherals.printing = with types; {
+    enable = mkBoolOpt false;
+    drivers = mkOpt (functionTo listOf package) (pkgs: [ ]);
+  };
+
+  config = mkIf cfg.enable {
+    services.printing = {
+      enable = true;
+      drivers = cfg.drivers pkgs;
+    };
+  };
+}
