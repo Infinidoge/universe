6fc7d912150b9a17b6a08c4088dea643d249c14a
Infinidoge <infinidoge@inx.moe>
2021-12-03 15:18:19 -0500

    devos/nix: add nix module

--- modules/devos/nix.nix
+++ modules/devos/nix.nix
@@ -1,5 +1,6 @@
-{ config, pkgs, self, lib, ... }: {
-
+{ channel, inputs, pkgs, lib, ... }:
+with lib;
+{
   nix = {
     package = pkgs.nixUnstable;
 
@@ -23,13 +24,24 @@
       keep-derivations = true
       fallback = true
     '';
+
+    nixPath = [
+      "nixpkgs=${channel.input}"
+      "nixos-config=${../../lib/compat/nixos}"
+      "home-manager=${inputs.home}"
+    ];
   };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
 
   environment = {
     systemPackages = with pkgs; [ nix-index nixfmt nixpkgs-fmt ];
 
     shellAliases =
-      let ifSudo = lib.mkIf config.security.sudo.enable;
+      let ifSudo = mkIf config.security.sudo.enable;
       in
       {
         # nix
@@ -49,14 +61,5 @@
         # fix nixos-option
         nixos-option = "nixos-option -I nixpkgs=${self}/lib/compat";
       };
-  };
-
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
-
-  home-manager = {
-    useUserPackages = true;
-    useGlobalPkgs = true;
   };
 }
