2e9c8a5662517029cfef4585b8bda842ed753ab2
Infinidoge <infinidoge@inx.moe>
2021-12-09 21:48:54 -0500

    hosts/Infini-STICK: force-load usb_storage

--- hosts/Infini-STICK/hardware-configuration.nix
+++ hosts/Infini-STICK/hardware-configuration.nix
@@ -11,8 +11,8 @@ in
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "ahci" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ "usb_storage" ];
   boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
   boot.extraModulePackages = [ ];
   boot.kernelParams = [ "boot.shell_on_fail" ];
