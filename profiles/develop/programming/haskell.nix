34a9c43e6a21931825e3938a71b8dcad51f21252
Infinidoge <infinidoge@inx.moe>
2021-12-25 02:15:57 -0500

    programming/haskell: add haskell dependencies

--- profiles/develop/programming/haskell.nix
+++ profiles/develop/programming/haskell.nix
@@ -2,6 +2,14 @@
   environment.systemPackages = with pkgs; lib.lists.flatten [
     haskell-language-server
     ghc
+    cabal-install
+
+    (with haskellPackages; [
+      hoogle
+
+      brittany
+      hls-brittany-plugin
+    ])
 
     stack
     # stack2nix
