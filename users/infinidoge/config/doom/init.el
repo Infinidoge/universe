9e1af74035d13c5fb069a07e94a16e5f63c2a162
Infinidoge <infinidoge@inx.moe>
2021-12-25 01:28:42 -0500

    config/doom: enable nim mode

--- users/infinidoge/config/doom/init.el
+++ users/infinidoge/config/doom/init.el
@@ -143,7 +143,7 @@
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
-       ;;nim               ; python + lisp at the speed of c
+       nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org ;; organize your plain life in plain text
