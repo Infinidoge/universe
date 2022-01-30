e0cd13ce46820cafb2051cbf364cb35c1a583e68
Infinidoge <infinidoge@inx.moe>
2022-01-30 17:01:10 -0500

    feat(&doom): enable :tools make

--- users/infinidoge/config/doom/init.el
+++ users/infinidoge/config/doom/init.el
@@ -94,7 +94,7 @@
        lookup              ; navigate your code and its documentation
        lsp
        (magit +forge)             ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make              ; run make tasks from Emacs
        (pass +auth)              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
