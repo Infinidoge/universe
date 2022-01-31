88977eff6b4bb7d346370f19c3e3a597c12fe5d7
Infinidoge <infinidoge@inx.moe>
2022-01-31 02:40:06 -0500

    feat(Infini-DESKTOP): migrate to opt-in state

---
+++ hosts/Infini-DESKTOP/install.bash
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# [[file:readme.org::install][install]]
+# [[[[file:/etc/nixos/hosts/Infini-DESKTOP/readme.org::mount][mount]]][mount]]
+# [[[[file:/etc/nixos/hosts/Infini-DESKTOP/readme.org::boilerplate][boilerplate]]][boilerplate]]
+DISK=$1
+PARTITION_PREFIX="p"
+
+sudo mkdir -p /mnt
+# boilerplate ends here
+
+# [[[[file:/etc/nixos/hosts/Infini-DESKTOP/readme.org::mount_check][mount_check]]][mount_check]]
+if mountpoint -q -- "/mnt"; then
+    echo "ERROR: /mnt is a mounted filesystem, aborting"
+    exit 1
+fi
+# mount_check ends here
+
+# [[[[file:/etc/nixos/hosts/Infini-DESKTOP/readme.org::mounting][mounting]]][mounting]]
+echo "LOG: Mounting tmpfs"
+sudo mount -t tmpfs root /mnt
+
+echo "LOG: - Mounting persistent directories"
+sudo mkdir -p /mnt/persist /mnt/nix /mnt/boot
+sudo mount -o subvol=root,autodefrag,noatime "${DISK}${PARTITION_PREFIX}2" /mnt/persist
+sudo mount -o subvol=nix,autodefrag,noatime "${DISK}${PARTITION_PREFIX}2" /mnt/nix
+sudo mount -o subvol=boot,autodefrag,noatime "${DISK}${PARTITION_PREFIX}2" /mnt/boot
+
+echo "LOG: - - Mounting persistent subdirectories"
+sudo mkdir -p /mnt/home
+sudo mount --bind /mnt/persist/home /mnt/home
+
+echo "LOG: - Mounting EFI System Partition"
+sudo mkdir -p /mnt/boot/efi
+sudo mount "${DISK}${PARTITION_PREFIX}1" /mnt/boot/efi
+# mounting ends here
+# mount ends here
+
+# [[[[file:/etc/nixos/hosts/Infini-DESKTOP/readme.org::installing][installing]]][installing]]
+echo "LOG: Installing NixOS"
+sudo nixos-install --flake /etc/nixos#Infini-DESKTOP --no-root-password
+# installing ends here
+
+# [[[[file:/etc/nixos/hosts/Infini-DESKTOP/readme.org::install_extra][install_extra]]][install_extra]]
+echo "LOG: Cloning configuration"
+sudo chown -R infinidoge /mnt/persist/etc/nixos
+git clone --no-hardlinks --progress https://gitlab.com/infinidoge/devos.git /mnt/persist/etc/nixos
+
+echo "LOG: Installing Doom Emacs"
+git clone --no-hardlinks --progress --depth 1 https://github.com/hlissner/doom-emacs /mnt/persist/home/infinidoge/.config/emacs
+HOME=/mnt/persist/home/infinidoge /mnt/persist/home/infinidoge/.config/emacs/bin/doom -y install --no-config
+# install_extra ends here
+
+# [[[[file:/etc/nixos/hosts/Infini-DESKTOP/readme.org::cleanup][cleanup]]][cleanup]]
+echo "LOG: Unmounting all"
+sudo umount -R /mnt
+# cleanup ends here
+# install ends here
---
+++ hosts/Infini-DESKTOP/mount.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# [[file:readme.org::mount][mount]]
+# [[[[file:/etc/nixos/hosts/Infini-DESKTOP/readme.org::boilerplate][boilerplate]]][boilerplate]]
+DISK=$1
+PARTITION_PREFIX="p"
+
+sudo mkdir -p /mnt
+# boilerplate ends here
+
+# [[[[file:/etc/nixos/hosts/Infini-DESKTOP/readme.org::mount_check][mount_check]]][mount_check]]
+if mountpoint -q -- "/mnt"; then
+    echo "ERROR: /mnt is a mounted filesystem, aborting"
+    exit 1
+fi
+# mount_check ends here
+
+# [[[[file:/etc/nixos/hosts/Infini-DESKTOP/readme.org::mounting][mounting]]][mounting]]
+echo "LOG: Mounting tmpfs"
+sudo mount -t tmpfs root /mnt
+
+echo "LOG: - Mounting persistent directories"
+sudo mkdir -p /mnt/persist /mnt/nix /mnt/boot
+sudo mount -o subvol=root,autodefrag,noatime "${DISK}${PARTITION_PREFIX}2" /mnt/persist
+sudo mount -o subvol=nix,autodefrag,noatime "${DISK}${PARTITION_PREFIX}2" /mnt/nix
+sudo mount -o subvol=boot,autodefrag,noatime "${DISK}${PARTITION_PREFIX}2" /mnt/boot
+
+echo "LOG: - - Mounting persistent subdirectories"
+sudo mkdir -p /mnt/home
+sudo mount --bind /mnt/persist/home /mnt/home
+
+echo "LOG: - Mounting EFI System Partition"
+sudo mkdir -p /mnt/boot/efi
+sudo mount "${DISK}${PARTITION_PREFIX}1" /mnt/boot/efi
+# mounting ends here
+# mount ends here
