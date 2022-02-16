3673aceaf3ac6ece01653a8ee2f86c8b8c9d16cd
Infinidoge <infinidoge@inx.moe>
2022-02-16 09:49:09 -0500

    feat(keychain): use keychain for ssh agent

---
+++ users/profiles/keychain.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+{
+  programs.keychain = {
+    enable = true;
+    extraFlags = [ "--quiet" "--dir ${config.xdg.configHome}/keychain" ];
+
+    keys = [ "id_ed25519" ];
+  };
+}
