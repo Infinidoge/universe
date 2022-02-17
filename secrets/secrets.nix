696f8718d93ed039dbda2ef7d6a9e8639cb6e1c8
Infinidoge <infinidoge@inx.moe>
2022-02-17 00:02:52 -0500

    feat(secrets): update keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,17 +1,14 @@
-{ lib, ... }:
+{ config, lib, ... }:
 with lib;
 let
   # set ssh public keys here for your system and user
   systems = {
-    Infini-DESKTOP = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9a6DiYuEeA+bX4rWgWGRUZwtln8sXtxCG9fPuvp9Hx root@Infini-DESKTOP";
+    Infini-DESKTOP = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID7uX1myj9ghv7wMoL038oGDCdScdyLd7RvYdnoioSBh root@Infini-DESKTOP";
     Infini-FRAMEWORK = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF7PmPq/7e+YIVAvIcs6EOJ3pZVJhinwus6ZauJ3aVp0 root@Infini-FRAMEWORK";
-    # Infini-RPI = "";
     # Infini-SERVER = "";
-    # Infini-STICK = "";
-    # Infini-SWIFT = "";
   };
   users = {
-    infinidoge = "";
+    infinidoge = config.users.users.infinidoge.openssh.authorizedKeys.keys;
   };
   allKeys = flatten [
     (attrValues systems)
