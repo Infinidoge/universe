654a7685a3f2b7bebfca67b6353b90496df4d354
Infinidoge <infinidoge@inx.moe>
2022-03-29 13:33:51 -0400

    nit(qtile): add icons to TODO

--- users/infinidoge/config/qtile/config.py
+++ users/infinidoge/config/qtile/config.py
@@ -597,7 +597,7 @@ def init_widget_list(main=True, laptop=False):
                                         padding=5,
                                     ),
                                 ],
-                                [
+                                [  # TODO: Create battery icon widget using NerdFont icons
                                     widget.Battery(
                                         format="{char} {percent:2.1%} {hour:d}h:{min:02d}m",
                                         update_interval=10,
