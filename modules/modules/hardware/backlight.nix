0d36b46eccd33db055a39dccb65ac7bc66667aae
Infinidoge <infinidoge@inx.moe>
2022-04-08 18:56:21 -0400

    module: add hardware/backlight

---
+++ modules/modules/hardware/backlight.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, inputs, ... }:
+with lib;
+with lib.our;
+with lib.hlissner;
+let
+  cfg = config.modules.hardware.backlight;
+in
+{
+  options.modules.hardware.backlight = with types; {
+    enable = mkBoolOpt false;
+    initialValue = mkOpt str "50%";
+    initialCommand = mkOpt str "${getMainProgram pkgs.brightnessctl} set ${cfg.initialValue}";
+  };
+
+  config.systemd.services = mkIf cfg.enable {
+    "set-initial-backlight" = {
+      description = "Sets the initial backlight status on startup";
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig.Type = "oneshot";
+      script = cfg.initialCommand;
+    };
+  };
+}
