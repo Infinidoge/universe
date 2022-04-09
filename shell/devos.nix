e048b7369ada126859a214d4652ad5b74b97f1c2
Infinidoge <infinidoge@inx.moe>
2022-04-09 03:02:53 -0400

    tweak(shell/devos): reenable nvfetcher

--- shell/devos.nix
+++ shell/devos.nix
@@ -33,12 +33,12 @@ in
   commands = with pkgs; [
     (devos nixUnstable)
     (devos agenix)
-    # {
-    #   category = "devos";
-    #   name = pkgs.nvfetcher-bin.pname;
-    #   help = pkgs.nvfetcher-bin.meta.description;
-    #   command = "cd $PRJ_ROOT/pkgs; ${pkgs.nvfetcher-bin}/bin/nvfetcher -c ./sources.toml $@";
-    # }
+    {
+      category = "devos";
+      name = pkgs.nvfetcher.pname;
+      help = pkgs.nvfetcher.meta.description;
+      command = "cd $PRJ_ROOT/pkgs; ${pkgs.nvfetcher}/bin/nvfetcher -c ./sources.toml $@";
+    }
     (linter nixpkgs-fmt)
     (linter editorconfig-checker)
     # (docs python3Packages.grip) too many deps
