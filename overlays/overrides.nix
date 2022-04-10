cfb03e39ed205d0bc89c456216c4b67f349fe07f
Infinidoge <infinidoge@inx.moe>
2022-04-09 21:05:33 -0400

    refactor(overlays): unpin devos packages from latest channel

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -3,24 +3,7 @@ channels: final: prev: {
   __dontExport = true; # overrides clutter up actual creations
 
   inherit (channels.latest)
-    cachix
-    deploy-rs
-    dhall
-    discord
     discord-canary
-    element-desktop
-    kitty
-    nixpkgs-fmt
-    omnisharp-roslyn
-    qutebrowser
-    rage
-    signal-desktop
-    starship
-    ;
-
-  inherit (channels.latest)
-    remarshal
-    spice-gtk
     ;
 
   inherit (channels.stable)
@@ -33,18 +16,6 @@ channels: final: prev: {
       overrides = prev.lib.composeExtensions (old.overrides or (_: _: { }))
         (py39final: py39prev: {
           inherit (channels.stable.python39Packages) pycurl;
-        });
-    });
-
-  haskellPackages = prev.haskellPackages.override
-    (old: {
-      overrides = prev.lib.composeExtensions (old.overrides or (_: _: { })) (hfinal: hprev:
-        let version = prev.lib.replaceChars [ "." ] [ "" ] prev.ghc.version;
-        in
-        {
-          # same for haskell packages, matching ghc versions
-          inherit (channels.latest.haskell.packages."ghc${version}")
-            haskell-language-server;
         });
     });
 }
