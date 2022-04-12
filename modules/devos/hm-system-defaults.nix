902ab319aab45c89b927f25270da84fd2017ba9a
Infinidoge <infinidoge@inx.moe>
2022-04-12 10:59:33 -0400

    tweak(hm-system-defaults): create nixpkgs/config.nix

--- modules/devos/hm-system-defaults.nix
+++ modules/devos/hm-system-defaults.nix
@@ -12,10 +12,16 @@
         home.sessionVariables = {
           inherit (config.environment.sessionVariables) NIX_PATH;
         };
-        xdg = {
-          configFile."nix/registry.json".text = config.environment.etc."nix/registry.json".text;
-          enable = true;
-        };
+        xdg =
+          {
+            configFile = {
+              "nix/registry.json".text = config.environment.etc."nix/registry.json".text;
+              "nixpkgs/config.nix".text = lib.generators.toPretty { } {
+                allowUnfree = true;
+              };
+            };
+            enable = true;
+          };
         home.stateVersion = config.system.stateVersion;
       }
       (lib.mkIf config.services.xserver.enable {
