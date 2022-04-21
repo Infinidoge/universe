4e76249eb18cf362a974d402dfae5030966c02e2
Infinidoge <infinidoge@inx.moe>
2022-04-20 23:45:31 -0400

    refactor(infinidoge): move ssh keys into separate file

--- users/infinidoge/default.nix
+++ users/infinidoge/default.nix
@@ -110,10 +110,6 @@ in
     ];
     shell = pkgs.zsh;
 
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAn6zCdIucse6eGvT3hFm7Unw9Qg6E37mAUE7HHL2d58 infinidoge@Infini-DESKTOP"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINwo8TGBe91mmkc/QonsXtTBKCJtsAGz3YzphDZlzmaO infinidoge@Infini-FRAMEWORK"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbNOMgVDM/hJQgzd1ff5uuouDtTLOAgmTt57cNNySif infinidoge@Infini-SERVER"
-    ];
+    openssh.authorizedKeys.keys = import ./ssh-keys.nix;
   };
 }
