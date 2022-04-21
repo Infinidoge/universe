a7ac082e866f3545dc07037495ed063fbb5f1657
Infinidoge <infinidoge@inx.moe>
2022-04-21 01:05:51 -0400

    fix(secrets): remove config/lib reference

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,14 +1,14 @@
-{ config, lib, ... }:
-with lib;
+with builtins;
 let
-  # set ssh public keys here for your system and user
+  flatten = x: if isList x then concatMap (y: flatten y) x else [ x ];
+
   systems = {
     Infini-DESKTOP = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID7uX1myj9ghv7wMoL038oGDCdScdyLd7RvYdnoioSBh root@Infini-DESKTOP";
     Infini-FRAMEWORK = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF7PmPq/7e+YIVAvIcs6EOJ3pZVJhinwus6ZauJ3aVp0 root@Infini-FRAMEWORK";
     Infini-SERVER = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8ptHWTesaUzglq01O8OVqeAGxFhXutUZpkgPpBFqzY root@Infini-SERVER";
   };
   users = {
-    infinidoge = config.users.users.infinidoge.openssh.authorizedKeys.keys;
+    infinidoge = import ../users/infinidoge/ssh-keys.nix;
   };
   allKeys = flatten [
     (attrValues systems)
