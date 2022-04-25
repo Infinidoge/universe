de5c01277ed31f83ffb13f32d908d572101751f0
Infinidoge <infinidoge@inx.moe>
2022-04-25 01:30:30 -0400

    feat(&minecraft): move minecraft to nix-minecraft repo

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,8 +2,4 @@ final: prev: {
   # keep sources this first
   sources = prev.callPackage (import ./_sources/generated.nix) { };
   # then, call packages with `final.callPackage`
-
-  fetchModrinthMod = final.callPackage (import ./fabric-mods/fetchModrinthMod.nix);
-
-  fabric-server = final.callPackage (import ./fabric-server) { };
 }
--- pkgs/fabric-mods/fetchModrinthMod.nix
+++
@@ -1,6 +0,0 @@
-{ stdenv, fetchurl, id, responseHash }:
-let
-  version = (builtins.fromJSON (builtins.readFile (fetchurl { url = "https://api.modrinth.com/v2/version/${id}"; sha256 = responseHash; })));
-  file = (builtins.elemAt version.files 0);
-in
-fetchurl { url = file.url; sha512 = file.hashes.sha512; }
