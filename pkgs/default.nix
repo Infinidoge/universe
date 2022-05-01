3a0649c2b58242ca577b7216e106cc3d88ccbb17
Infinidoge <infinidoge@inx.moe>
2022-05-01 00:02:02 -0400

    feat(pkgs): update kmscon

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,4 +2,9 @@ final: prev: {
   # keep sources this first
   sources = prev.callPackage (import ./_sources/generated.nix) { };
   # then, call packages with `final.callPackage`
+
+  kmscon = prev.kmscon.overrideAttrs (old: {
+    version = "unstable-2022-04-28";
+    src = final.sources.kmscon.src;
+  });
 }
