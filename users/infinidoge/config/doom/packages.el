7beb908123dab50f3a99129f605675eda07bc2b0
Infinidoge <infinidoge@inx.moe>
2022-05-02 00:05:30 -0400

    refactor(doom): remove lsp-python-ms
    Unnecessary due to using pyright now

--- users/infinidoge/config/doom/packages.el
+++ users/infinidoge/config/doom/packages.el
@@ -48,7 +48,3 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
-
-;; (package! org-auto-tangle)
-
-(package! lsp-python-ms)
