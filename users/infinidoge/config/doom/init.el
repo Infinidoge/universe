ee8d5b2da7379b00639c453ad57079114d2ba20e
Infinidoge <infinidoge@inx.moe>
2022-05-02 00:09:44 -0400

    refactor(doom): move evil-goggles tweak into separate file

--- users/infinidoge/config/doom/init.el
+++ users/infinidoge/config/doom/init.el
@@ -196,11 +196,4 @@
  )
 
 
-(use-package-hook! evil-goggles
-  :post-init
-  (setq
-   evil-goggles-duration 0.1
-   evil-goggles-pulse nil
-   evil-goggles-enable-change t
-   evil-goggles-enable-delete t)
-  t)
+(load! "init/evil-goggles")
