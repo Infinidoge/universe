ada7f27dc0620375cfe20666a5cdbe88a1c16fe7
Infinidoge <infinidoge@inx.moe>
2022-05-27 01:10:41 -0400

    tweak(cachix/ssh): route through tailscale

--- modules/devos/cachix/ssh.nix
+++ modules/devos/cachix/ssh.nix
@@ -2,6 +2,6 @@
 {
   nix.settings.substituters = lib.mkIf (config.networking.hostName != "Infini-DESKTOP")
     ((if config.info.loc.home then (lib.mkOrder 300) else lib.mkAfter) [
-      "ssh://server.doge-inc.net"
+      "ssh://infini-desktop"
     ]);
 }
