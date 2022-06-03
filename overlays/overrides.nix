eaaf2db5818c064d05b7baa95280dba5f035111b
Infinidoge <infinidoge@inx.moe>
2022-06-03 01:37:42 -0400

    revert: fix(devshell): pin pycurl to stable

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -9,15 +9,6 @@ channels: final: prev: {
     polymc
     ;
 
-  # https://github.com/NixOS/nixpkgs/issues/167579
-  python39Packages = prev.python39Packages.override
-    (old: {
-      overrides = prev.lib.composeExtensions (old.overrides or (_: _: { }))
-        (py39final: py39prev: {
-          inherit (channels.latest.python39Packages) pycurl;
-        });
-    });
-
   qtile = prev.qtile.override {
     python3 = final.python310;
     python3Packages = final.python310Packages;
