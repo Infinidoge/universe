fac6016aac5c3399353590a0f2e9e59128b69775
Infinidoge <infinidoge@inx.moe>
2022-06-22 22:47:16 -0400

    fix(shells/nu): update nu config, reenable

--- users/profiles/shells/nushell.nix
+++ users/profiles/shells/nushell.nix
@@ -1,15 +1,12 @@
 { ... }: {
   programs.nushell = {
     enable = true;
-    settings = {
-      complete_from_path = true;
-      ctrlc_exit = true;
-      startup = [
-        "mkdir ~/.cache/starship"
-        "starship init nu | save ~/.cache/starship/init.nu"
-        "source ~/.cache/starship/init.nu"
-      ];
-      prompt = "starship_prompt";
-    };
+    configFile.text = ''
+      source ~/.cache/starship/init.nu
+    '';
+    envFile.text = ''
+      mkdir ~/.cache/starship
+      starship init nu | save ~/.cache/starship/init.nu
+    '';
   };
 }
