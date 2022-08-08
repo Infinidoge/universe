54c3b6bd41435afc96a85bd5b6abe367c212816d
Infinidoge <infinidoge@inx.moe>
2022-08-08 04:26:02 -0400

    feat(doom,programming): add java setup

--- users/infinidoge/config/doom/init.el
+++ users/infinidoge/config/doom/init.el
@@ -136,7 +136,7 @@
  ;;hy                           ; readability of scheme w/ speed of python
  ;;idris                        ; a language you can depend on
  json                           ; At least it ain't XML
- ;;(java +meghanada)            ; the poster child for carpal tunnel syndrome
+ (java +meghanada +tree-sitter)                 ; the poster child for carpal tunnel syndrome
  ;;javascript                   ; all(hope(abandon(ye(who(enter(here))))))
  ;;julia                        ; a better, faster MATLAB
  ;;kotlin                       ; a better, slicker Java(Script)
