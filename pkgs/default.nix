b576a38c7cab020e9e5b001f96d1952ed04266da
Infinidoge <infinidoge@inx.moe>
2022-08-12 03:20:59 -0400

    refactor(pkgs/frei): use nix file for package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,8 +5,5 @@ final: prev: {
 
   olympus = prev.callPackage ./olympus.nix { };
 
-  frei = prev.buildGo118Module {
-    inherit (final.sources.frei) pname version src;
-    vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
-  };
+  frei = prev.callPackage ./frei.nix { source = final.sources.frei; };
 }
