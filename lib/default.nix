ff6a1940d6e5f932317cc479684c707a1de34d91
Infinidoge <infinidoge@inx.moe>
2022-08-27 13:05:20 -0400

    nit(lib): reformat

--- lib/default.nix
+++ lib/default.nix
@@ -1,35 +1,32 @@
 { lib }:
 lib.makeExtensible (self:
+with lib;
+rec {
+  flattenListSet = imports: (flatten (concatLists (attrValues imports)));
+  flattenSetList = attrSet: (mapAttrs (name: value: flatten value) attrSet);
 
-  with lib;
-  rec {
-    flattenListSet = imports: (flatten (concatLists (attrValues imports)));
-    flattenSetList = attrSet: (mapAttrs (name: value: flatten value) attrSet);
+  # ["/home/user/" "/.screenrc"] -> ["home" "user" ".screenrc"]
+  splitPath = paths:
+    (filter
+      (s: builtins.typeOf s == "string" && s != "")
+      (concatMap (builtins.split "/") paths)
+    );
 
+  # ["home" "user" ".screenrc"] -> "home/user/.screenrc"
+  dirListToPath = dirList: (concatStringsSep "/" dirList);
 
-    # ["/home/user/" "/.screenrc"] -> ["home" "user" ".screenrc"]
-    splitPath = paths:
-      (filter
-        (s: builtins.typeOf s == "string" && s != "")
-        (concatMap (builtins.split "/") paths)
-      );
+  # ["/home/user/" "/.screenrc"] -> "/home/user/.screenrc"
+  concatPaths = paths:
+    let
+      prefix = optionalString (hasPrefix "/" (head paths)) "/";
+      path = dirListToPath (splitPath paths);
+    in
+    prefix + path;
 
-    # ["home" "user" ".screenrc"] -> "home/user/.screenrc"
-    dirListToPath = dirList: (concatStringsSep "/" dirList);
+  sanitizeName = name:
+    replaceStrings
+      [ "." ] [ "" ]
+      (strings.sanitizeDerivationName (removePrefix "/" name));
 
-    # ["/home/user/" "/.screenrc"] -> "/home/user/.screenrc"
-    concatPaths = paths:
-      let
-        prefix = optionalString (hasPrefix "/" (head paths)) "/";
-        path = dirListToPath (splitPath paths);
-      in
-      prefix + path;
-
-    sanitizeName = name:
-      replaceStrings
-        [ "." ] [ "" ]
-        (strings.sanitizeDerivationName (removePrefix "/" name));
-
-    getMainProgram = package: "${package}/bin/${attrByPath ["meta" "mainProgram"] package.pname package}";
-  }
-)
+  getMainProgram = package: "${package}/bin/${attrByPath ["meta" "mainProgram"] package.pname package}";
+})
