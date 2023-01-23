fb4b537eea1026ad4b2851e2cf31781666ab2e32
Infinidoge <infinidoge@inx.moe>
2023-01-23 02:20:05 -0500

    fix(networking): explicitly define DNS settings

--- modules/global/networking.nix
+++ modules/global/networking.nix
@@ -3,10 +3,30 @@
   networking = {
     useDHCP = true;
     firewall = {
-      checkReversePath = "loose";
       trustedInterfaces = [ "tailscale0" ];
       allowedUDPPorts = [ config.services.tailscale.port ];
     };
+    search = [
+      # Tailscale
+      "tail4c593.ts.net"
+      "infinidoge.github.beta.tailscale.net"
+    ];
+    nameservers = [
+      # Tailscale
+      "100.100.100.100"
+
+      # Google Public DNS
+      "8.8.8.8"
+      "8.8.4.4"
+      "2001:4860:4860::8888"
+      "2001:4860:4860::8844"
+
+      # Cloudflare Public DNS
+      "1.1.1.1"
+      "1.0.0.1"
+      "2696:4700:4700::1111"
+      "2696:4700:4700::1111"
+    ];
   };
 
   services = {
@@ -22,6 +42,9 @@
       };
     };
 
-    tailscale.enable = true;
+    tailscale = {
+      enable = true;
+      useRoutingFeatures = if config.info.stationary then "both" else "client";
+    };
   };
 }
