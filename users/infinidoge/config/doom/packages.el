de0f283537c1b0dbdb1033e5b0d13c263697ea81
Infinidoge <infinidoge@inx.moe>
2023-02-15 16:07:13 -0500

    fix(doom): pin transient & with-editor
    Without the pin, Doom will not start

--- users/infinidoge/config/doom/packages.el
+++ users/infinidoge/config/doom/packages.el
@@ -51,3 +51,6 @@
 
 (package! emacsql-sqlite :built-in 'prefer)
 (package! pdf-tools :built-in 'prefer)
+
+(package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
+(package! with-editor :pin "391e76a256aeec6b9e4cbd733088f30c677d965b")
