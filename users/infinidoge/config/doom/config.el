680887575ca690e5ce61fa01eeb5ca0d30966729
Infinidoge <infinidoge@inx.moe>
2023-02-23 11:45:02 -0500

    tweak(doom): remove unnecessary langtool fix

--- users/infinidoge/config/doom/config.el
+++ users/infinidoge/config/doom/config.el
@@ -67,12 +67,6 @@
 ;; --- GnuPG ---
 (setq epg-pinentry-mode 'loopback)
 
-;; --- langtool ---
-(use-package! langtool
-  :config
-  (setq langtool-bin "languagetool-commandline")
-  )
-
 ;; --- Evil Goggles ---
 (use-package! evil-goggles
   :config
