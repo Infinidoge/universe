506295be38ad936f6447fa76eb86d8875ecd1faa
Infinidoge <infinidoge@inx.moe>
2023-04-06 11:04:25 -0400

    bump: qtile

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -3,14 +3,14 @@
 {
   qtile = {
     pname = "qtile";
-    version = "4c69fca361bcb1d9753b37b9b903af32b27983c5";
+    version = "e88537fbcce9e8e0de3349c566de4fda9a225e96";
     src = fetchFromGitHub ({
       owner = "qtile";
       repo = "qtile";
-      rev = "4c69fca361bcb1d9753b37b9b903af32b27983c5";
+      rev = "e88537fbcce9e8e0de3349c566de4fda9a225e96";
       fetchSubmodules = false;
-      sha256 = "sha256-c/1+17k+kx5M9c/VcXA6+o9GWzbM69IJkhCbK8NEdlc=";
+      sha256 = "sha256-FEiI7T4whhClb8nCrk+CxlihyMVuM3c6EcnmttPszAU=";
     });
-    date = "2023-02-27";
+    date = "2023-03-31";
   };
 }
