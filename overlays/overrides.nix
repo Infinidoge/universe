5a578ae32fcacbacc2fb06864a14d4025fba8056
Infinidoge <infinidoge@inx.moe>
2023-05-08 02:02:20 -0400

    overrides: repin discord-canary to fork
    Cherry-picked commits that allow for installing Vencord

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -3,9 +3,14 @@ channels: final: prev: {
   __dontExport = true; # overrides clutter up actual creations
 
   inherit (channels.latest)
-    discord-canary
+    # discord-canary
     kmscon
     prismlauncher
+    ;
+
+  inherit (channels.fork)
+    # https://nixpk.gs/pr-tracker.html?pr=229173
+    discord-canary
     ;
 
   inherit (channels.stable)
