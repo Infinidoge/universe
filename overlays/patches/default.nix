3e8999c88f26426ae83cb9f57e80ea03ef393b36
Infinidoge <infinidoge@inx.moe>
2023-06-18 12:43:22 -0400

    fix(patches): disable nitter patch

--- overlays/patches/default.nix
+++ overlays/patches/default.nix
@@ -3,12 +3,12 @@ final: prev: {
     patches = [ ./coreutils.patch ];
   });
 
-  nitter = prev.nitter.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [
-      (final.fetchpatch {
-        url = "https://patch-diff.githubusercontent.com/raw/zedeus/nitter/pull/830.patch";
-        hash = "sha256-MIl22yWHAGQtcB1/B9OfbfoVhxOBIAJ3n1eT2Ko1Y3Q=";
-      })
-    ];
-  });
+  # nitter = prev.nitter.overrideAttrs (old: {
+  #   patches = (old.patches or [ ]) ++ [
+  #     (final.fetchpatch {
+  #       url = "https://patch-diff.githubusercontent.com/raw/zedeus/nitter/pull/830.patch";
+  #       hash = "sha256-MIl22yWHAGQtcB1/B9OfbfoVhxOBIAJ3n1eT2Ko1Y3Q=";
+  #     })
+  #   ];
+  # });
 }
