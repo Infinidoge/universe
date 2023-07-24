4c275b2d3cac251d2328e2db9fcf9f0643d61610
Infinidoge <infinidoge@inx.moe>
2023-07-24 15:18:32 -0400

    pkgs: reorganise callPackage lines

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,19 +3,12 @@ final: prev: {
   sources = prev.callPackage (import ./_sources/generated.nix) { };
   # then, call packages with `final.callPackage`
 
-  olympus = final.callPackage ./olympus.nix { };
-
-  nix-modrinth-prefetch = final.callPackage ./nix-modrinth-prefetch.nix { };
-
-  qtile-unstable = final.callPackage ./qtile.nix { source = final.sources.qtile; };
-
   mcaselector = final.callPackage ./mcaselector.nix { };
-
-  sim65 = final.callPackage ./sim65.nix { };
-
-  unbted = final.callPackage ./unbted.nix { };
-
+  nix-modrinth-prefetch = final.callPackage ./nix-modrinth-prefetch.nix { };
+  olympus = final.callPackage ./olympus.nix { };
+  qtile-unstable = final.callPackage ./qtile.nix { source = final.sources.qtile; };
   setris = final.callPackage ./setris.nix { };
-
+  sim65 = final.callPackage ./sim65.nix { };
   substituteSubset = final.callPackage ./substitute-subset.nix { };
+  unbted = final.callPackage ./unbted.nix { };
 }
