c3c58022cef31d9c158c22fa2b3d8c6db9d5eb13
Infinidoge <infinidoge@inx.moe>
2023-07-25 19:43:21 -0400

    overlays/patches: fix kitty showing window size on resize

--- overlays/patches/default.nix
+++ overlays/patches/default.nix
@@ -8,6 +8,17 @@ in
 {
   coreutils-doge = addPatches prev.coreutils [ ./coreutils.patch ];
 
+  kitty = addPatches prev.kitty [
+    (final.fetchpatch {
+      url = "https://github.com/kovidgoyal/kitty/commit/e50ab57b8d9c7b78d0ac106c33b44ea55ae86f0b.patch";
+      hash = "sha256-S4/BWRj3cpL72Gqb0PcodmZmoIJz+faZV7kcb6X2SmI=";
+    })
+    (final.fetchpatch {
+      url = "https://github.com/kovidgoyal/kitty/commit/601e502d2e27baca7446587cb5188c965eaf3789.patch";
+      hash = "sha256-ugnjiiBWopLiAtf9KytjWEk7NH4IdVtq8Zj3GTaJLxs=";
+    })
+  ];
+
   # nitter = prev.nitter.overrideAttrs (old: {
   #   patches = (old.patches or [ ]) ++ [
   #     (final.fetchpatch {
