d0e77aabe22d35767f718fac7f9a7851f5d9b24c
Infinidoge <infinidoge@inx.moe>
2023-08-21 16:13:22 -0400

    pkgs/neocities: init at 0.0.18

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -3,6 +3,7 @@
   ears-cli = pkgs.callPackage ./ears-cli.nix { };
   hexagon = pkgs.callPackage ./hexagon.nix { };
   mcaselector = pkgs.callPackage ./mcaselector.nix { };
+  neocities = pkgs.callPackage ./neocities { };
   nix-modrinth-prefetch = pkgs.callPackage ./nix-modrinth-prefetch.nix { };
   olympus = pkgs.callPackage ./olympus.nix { };
   setris = pkgs.callPackage ./setris.nix { };
