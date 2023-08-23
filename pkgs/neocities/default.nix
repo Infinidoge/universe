1f950e1c2605d754ca3cca9e008734e0f848c2b3
Infinidoge <infinidoge@inx.moe>
2023-08-23 09:58:05 -0400

    pkgs/neocities: add to metadata

--- pkgs/neocities/default.nix
+++ pkgs/neocities/default.nix
@@ -7,6 +7,10 @@ bundlerApp {
   exes = [ "neocities" ];
 
   meta = with lib; {
+    description = "The Neocities CLI, written in Ruby";
+    homepage = "https://github.com/neocities/neocities-ruby";
+    license = licenses.mit;
+    maintainers = with maintainers; [ infinidoge ];
     mainProgram = "neocities";
   };
 }
