ee2891fee15d0a0b2685f339dd6e251e28b6ef1a
Infinidoge <infinidoge@inx.moe>
2023-08-31 13:27:44 -0400

    Infini-SERVER: regenerate bash scripts

--- hosts/Infini-SERVER/bare_install.bash
+++ hosts/Infini-SERVER/bare_install.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # [[file:readme.org::bare_install][bare_install]]
-# [[[[file:/etc/nixos/hosts/Infini-SERVER/readme.org::installing][installing]]][installing]]
+# [[file:readme.org::bare_install][installing]]
 echo "LOG: Installing NixOS"
 sudo nixos-install --flake /etc/nixos#Infini-SERVER --no-root-password
 # installing ends here
