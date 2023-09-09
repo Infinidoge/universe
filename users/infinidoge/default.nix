6d85049800a534cf0d47c5a60ed5f777e9660a55
Infinidoge <infinidoge@inx.moe>
2023-09-08 23:05:19 -0400

    infinidoge: make username overridable
    For cases where I am configuring hosts where it needs to be different.

--- users/infinidoge/default.nix
+++ users/infinidoge/default.nix
@@ -84,7 +84,7 @@ in
   };
 
   user = {
-    name = "infinidoge";
+    name = lib.mkDefault "infinidoge";
     uid = 1000;
     passwordFile = mkIf config.modules.secrets.enable config.secrets.infinidoge-password;
     description = "Infinidoge, primary user of the system";
