738e384f23c3d125730cdfb9b47fd6cdb27efe8b
Infinidoge <infinidoge@inx.moe>
2023-09-14 16:02:42 -0400

    shells/nushell: remove redundant/broken starship init

--- users/modules/global/shells/nushell.nix
+++ users/modules/global/shells/nushell.nix
@@ -1,12 +1,5 @@
 { ... }: {
   programs.nushell = {
     enable = true;
-    configFile.text = ''
-      source ~/.cache/starship/init.nu
-    '';
-    envFile.text = ''
-      mkdir ~/.cache/starship
-      starship init nu | save ~/.cache/starship/init.nu
-    '';
   };
 }
