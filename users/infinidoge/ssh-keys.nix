6298cd8ea952770eb1f37100cfa005b32e788f15
Infinidoge <infinidoge@inx.moe>
2023-10-19 19:09:45 -0400

    flake: add Infini-OPTIPLEX ssh keys

--- users/infinidoge/ssh-keys.nix
+++ users/infinidoge/ssh-keys.nix
@@ -2,6 +2,7 @@
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAn6zCdIucse6eGvT3hFm7Unw9Qg6E37mAUE7HHL2d58 infinidoge@Infini-DESKTOP"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINwo8TGBe91mmkc/QonsXtTBKCJtsAGz3YzphDZlzmaO infinidoge@Infini-FRAMEWORK"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbNOMgVDM/hJQgzd1ff5uuouDtTLOAgmTt57cNNySif infinidoge@Infini-SERVER"
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBfpzp4nROMc3nuSu+/ivkm0koyn2Da5NtVbYl+Rg+O1 infinidoge@Infini-OPTIPLEX"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBE2YScxTK7JOCXBn4KjFWp9Lwcp4OovesYXrPCpRP4X Project Infinity"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIltuTbxiBxOox2DAW5LACjGjJ9UFvJOAcrFYK7WMHkV [redacted-infinidoge]@data.cs.purdue.edu"
 ]
