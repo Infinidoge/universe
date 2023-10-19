2bbe980d9f9433df8eccd7fe216c15ff92023d91
Infinidoge <infinidoge@inx.moe>
2023-10-19 19:09:45 -0400

    flake: add Infini-OPTIPLEX ssh keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,6 +6,7 @@ let
     Infini-DESKTOP = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID7uX1myj9ghv7wMoL038oGDCdScdyLd7RvYdnoioSBh root@Infini-DESKTOP";
     Infini-FRAMEWORK = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF7PmPq/7e+YIVAvIcs6EOJ3pZVJhinwus6ZauJ3aVp0 root@Infini-FRAMEWORK";
     Infini-SERVER = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8ptHWTesaUzglq01O8OVqeAGxFhXutUZpkgPpBFqzY root@Infini-SERVER";
+    Infini-OPTIPLEX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEG8fY684SPKeOUsJqaV6LJwwztWxztaU9nAHPBxBtyU root@Infini-OPTIPLEX";
   };
   users = {
     infinidoge = import ../users/infinidoge/ssh-keys.nix;
