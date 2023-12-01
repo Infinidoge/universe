510fc4ae671b020326ebe9bf0a7c612ba0e4fe6d
Infinidoge <infinidoge@inx.moe>
2023-12-01 11:00:39 -0500

    caches/ssh: enable Infini-DESKTOP cache outside of home

--- modules/global/caches/ssh.nix
+++ modules/global/caches/ssh.nix
@@ -1,7 +1,7 @@
 { config, lib, ... }:
 {
   nix.settings = {
-    substituters = lib.mkIf (config.networking.hostName != "Infini-DESKTOP" && config.info.loc.home)
+    substituters = lib.mkIf (config.networking.hostName != "Infini-DESKTOP")
       ((if config.info.loc.home then (lib.mkOrder 300) else lib.mkAfter) [
         "ssh://infini-desktop"
       ]);
