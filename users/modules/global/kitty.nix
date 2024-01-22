fed22513045df6d40561022e52166d86d53176f9
Infinidoge <infinidoge@inx.moe>
2024-01-22 18:01:46 -0500

    global/kitty,mpd: disable when not in graphical environment

--- users/modules/global/kitty.nix
+++ users/modules/global/kitty.nix
@@ -1,19 +1,22 @@
-{ pkgs, ... }: {
-  programs.kitty = {
-    enable = true;
-    font = {
-      package = (pkgs.nerdfonts.override { fonts = [ "DejaVuSansMono" ]; });
-      name = "DejaVuSansMono";
-      size = 12;
+{ main, pkgs, lib, ... }:
+{
+  config = lib.mkIf main.info.graphical {
+    programs.kitty = {
+      enable = true;
+      font = {
+        package = (pkgs.nerdfonts.override { fonts = [ "DejaVuSansMono" ]; });
+        name = "DejaVuSansMono";
+        size = 12;
+      };
+      settings = {
+        confirm_os_window_close = 0;
+        scrollback_lines = 100000;
+      };
     };
-    settings = {
-      confirm_os_window_close = 0;
-      scrollback_lines = 100000;
-    };
-  };
 
-  home.shellAliases = {
-    ssh = "kitty +kitten ssh";
-    icat = "kitty +kitten icat";
+    home.shellAliases = {
+      ssh = "kitty +kitten ssh";
+      icat = "kitty +kitten icat";
+    };
   };
 }
