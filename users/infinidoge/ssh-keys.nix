d67462768525a9b9609be5d62dda75ec8bdfcdcf
Infinidoge <infinidoge@inx.moe>
2024-01-22 21:49:00 -0500

    users/infinidoge: add Infini-STICK key and rekey secrets

--- users/infinidoge/ssh-keys.nix
+++ users/infinidoge/ssh-keys.nix
@@ -3,6 +3,7 @@
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINwo8TGBe91mmkc/QonsXtTBKCJtsAGz3YzphDZlzmaO infinidoge@Infini-FRAMEWORK"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbNOMgVDM/hJQgzd1ff5uuouDtTLOAgmTt57cNNySif infinidoge@Infini-SERVER"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBfpzp4nROMc3nuSu+/ivkm0koyn2Da5NtVbYl+Rg+O1 infinidoge@Infini-OPTIPLEX"
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJvXocQzoHqluU+eRhb68N77Avzux1IY9pe/0tl7QV3a infinidoge@Infini-STICK"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBE2YScxTK7JOCXBn4KjFWp9Lwcp4OovesYXrPCpRP4X Project Infinity"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIltuTbxiBxOox2DAW5LACjGjJ9UFvJOAcrFYK7WMHkV [redacted-infinidoge]@data.cs.purdue.edu"
 ]
