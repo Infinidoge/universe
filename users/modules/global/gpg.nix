ad54e9947b6cfd7db33f76834609e02973ff5012
Infinidoge <infinidoge@inx.moe>
2024-01-24 03:24:09 -0500

    global/gpg: setup with new PGP key

--- users/modules/global/gpg.nix
+++ users/modules/global/gpg.nix
@@ -1,10 +1,24 @@
-{ config, main, ... }: {
+{ config, main, pkgs, ... }:
+{
   programs.gpg = {
     enable = true;
     homedir = "${config.xdg.dataHome}/gnupg";
+
+    settings = {
+      no-comments = false;
+      comment = [
+        "Key URL: https://inx.moe/pub.txt"
+        "Website: https://inx.moe"
+      ];
+    };
+
     scdaemonSettings = {
       disable-ccid = true;
     };
+
+    publicKeys = [
+      { source = pkgs.fetchurl { url = "https://inx.moe/pub.txt"; sha256 = "sha256-QLxmqS5fbR3zqKNvFV+K22XeLuNfrSp2JxBDQtqgTiE="; }; trust = "ultimate"; }
+    ];
   };
 
   services.gpg-agent = {
