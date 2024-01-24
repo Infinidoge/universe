4d74223b8d145733028d22dc25f6d47572dda170
Infinidoge <infinidoge@inx.moe>
2024-01-24 03:18:13 -0500

    users/infinidoge: add primary roaming key to ssh keys

--- users/infinidoge/ssh-keys.nix
+++ users/infinidoge/ssh-keys.nix
@@ -1,4 +1,5 @@
 [
+  "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPVs2Acpv3mHdBZElauxgoqr9Sd9D/q/EazCGg1uSgdPAAAAHnNzaDppbmZpbmlkb2dlLXJvYW1pbmctcHJpbWFyeQ== ssh:infinidoge-roaming-primary"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAn6zCdIucse6eGvT3hFm7Unw9Qg6E37mAUE7HHL2d58 infinidoge@Infini-DESKTOP"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINwo8TGBe91mmkc/QonsXtTBKCJtsAGz3YzphDZlzmaO infinidoge@Infini-FRAMEWORK"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbNOMgVDM/hJQgzd1ff5uuouDtTLOAgmTt57cNNySif infinidoge@Infini-SERVER"
