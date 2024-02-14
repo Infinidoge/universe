56761a06db72f9f06fb266aa514bc1ba2d30b711
Infinidoge <infinidoge@inx.moe>
2024-02-14 01:41:24 -0500

    global/networking: remove avahi

--- modules/global/networking.nix
+++ modules/global/networking.nix
@@ -32,18 +32,6 @@
   };
 
   services = {
-    avahi = {
-      enable = true;
-      nssmdns4 = true;
-      publish = {
-        enable = true;
-        userServices = true;
-      };
-      extraServiceFiles = {
-        ssh = "${pkgs.avahi}/etc/avahi/services/ssh.service";
-      };
-    };
-
     tailscale = {
       enable = true;
       useRoutingFeatures = if config.info.stationary then "both" else "client";
