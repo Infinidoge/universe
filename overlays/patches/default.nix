91a29faaa77b899f62dfe0004f25c38ae58e3059
Infinidoge <infinidoge@inx.moe>
2024-02-14 16:59:28 -0500

    patches: remove commented nitter patch

--- overlays/patches/default.nix
+++ overlays/patches/default.nix
@@ -7,13 +7,4 @@ let
 in
 {
   coreutils-doge = addPatches prev.coreutils [ ./coreutils.patch ];
-
-  # nitter = prev.nitter.overrideAttrs (old: {
-  #   patches = (old.patches or [ ]) ++ [
-  #     (final.fetchpatch {
-  #       url = "https://patch-diff.githubusercontent.com/raw/zedeus/nitter/pull/830.patch";
-  #       hash = "sha256-MIl22yWHAGQtcB1/B9OfbfoVhxOBIAJ3n1eT2Ko1Y3Q=";
-  #     })
-  #   ];
-  # });
 }
