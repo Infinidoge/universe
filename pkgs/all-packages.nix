b43b922e787066b1a6691265186ec7cc2f535564
Infinidoge <infinidoge@inx.moe>
2024-02-19 13:56:36 -0500

    pkgs/mkSymlinks: add method to create arbitrary symlinks

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -3,6 +3,7 @@
   ears-cli = pkgs.callPackage ./ears-cli.nix { };
   hexagon = pkgs.callPackage ./hexagon.nix { };
   mcaselector = pkgs.callPackage ./mcaselector.nix { };
+  mkSymlinks = pkgs.callPackage ./mk-symlinks.nix { };
   neocities = pkgs.callPackage ./neocities { };
   nix-modrinth-prefetch = pkgs.callPackage ./nix-modrinth-prefetch.nix { };
   olympus = pkgs.callPackage ./olympus.nix { };
