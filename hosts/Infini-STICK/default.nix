9ef0dbfb249e7e58d53fdacd77d40566f1ba9600
Infinidoge <infinidoge@inx.moe>
2024-02-26 23:50:24 -0500

    Infini-STICK: enable support for ZFS

--- hosts/Infini-STICK/default.nix
+++ hosts/Infini-STICK/default.nix
@@ -1,8 +1,12 @@
-{ lib, ... }: {
+{ lib, config, ... }: {
   imports = [
     ./hardware-configuration.nix
     ./filesystems.nix
   ];
+
+  networking.hostId = "06a3f197";
+
+  boot.kernelPackages = lib.mkForce config.boot.zfs.package.latestCompatibleLinuxPackages;
 
   modules = {
     boot.grub.enable = true;
