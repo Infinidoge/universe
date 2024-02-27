13f6258db5a7cb724e53420e0dc663914562ff75
Infinidoge <infinidoge@inx.moe>
2024-02-27 03:04:12 -0500

    Infini-SD: init

---
+++ hosts/Infini-SD/filesystems.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+
+let
+  uuid = uuid: "/dev/disk/by-uuid/${uuid}";
+  main = uuid "ae3f3d98-1d87-47b4-a4ed-d69a896eee69";
+  commonOptions = [ "autodefrag" "noatime" "compress-force=zstd:4" ];
+
+  mkMain' = options: {
+    device = main;
+    fsType = "btrfs";
+    options = commonOptions ++ options;
+  };
+  mkMain = options: (mkMain' options) // { neededForBoot = true; };
+in
+{
+  fileSystems = {
+    "/" = {
+      device = "none";
+      fsType = "tmpfs";
+      options = [ "defaults" "size=4G" "mode=755" ];
+    };
+
+    "/media/main" = mkMain' [ ];
+
+    "/persist" = mkMain [ "subvol=root" ];
+    "/etc/ssh" = mkMain [ "subvol=root/etc/ssh" ];
+    "/nix" = mkMain [ "subvol=nix" ];
+    "/boot" = mkMain [ "subvol=boot" ];
+
+    "/boot/efi" = {
+      device = uuid "D7DB-2291";
+      fsType = "vfat";
+      neededForBoot = true;
+    };
+  };
+}
---
+++ hosts/Infini-SD/hardware-configuration.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "ahci" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ "usb_storage" ];
+  boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+  boot.kernelParams = [ "boot.shell_on_fail" ];
+  boot.supportedFilesystems = [ "btrfs" "zfs" ];
+  boot.zfs.forceImportRoot = false;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+
+  info.model = "Portable Installation";
+}
