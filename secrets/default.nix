296461b0bc285f9a4e781ae5b48e673150c841d6
Infinidoge <infinidoge@inx.moe>
2024-03-25 11:02:45 -0400

    secrets: improve formatting

--- secrets/default.nix
+++ secrets/default.nix
@@ -4,7 +4,9 @@ let
   inherit (lib.our) mkOpt;
   inherit (lib.types) bool attrsOf path;
 
-  mkSecret = name: nameValuePair (removeSuffix ".age" name) { file = "${./.}/${name}"; };
+  mkSecret = name: nameValuePair
+    (removeSuffix ".age" name)
+    { file = "${./.}/${name}"; };
   secrets = listToAttrs (map mkSecret (attrNames (import ./secrets.nix)));
 
   withOwner = name: secret: secret // { owner = name; group = name; };
@@ -18,7 +20,13 @@ in
   config = mkIf config.modules.secrets.enable {
     secrets = mapAttrs (n: v: v.path) config.age.secrets;
     age.secrets = mkMerge [
-      { inherit (secrets) "infinidoge-password" "root-password" "binary-cache-private-key"; }
+      {
+        inherit (secrets)
+          "infinidoge-password"
+          "root-password"
+          "binary-cache-private-key"
+          ;
+      }
       (mkIf config.services.nginx.enable {
         "inx.moe.pem" = withOwner "nginx" secrets."inx.moe.pem";
         "inx.moe.key" = withOwner "nginx" secrets."inx.moe.key";
