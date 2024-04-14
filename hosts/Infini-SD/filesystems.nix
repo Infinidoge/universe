316c4e7a4a461799c44ddfd34d9313dfba8a3ec3
Infinidoge <infinidoge@inx.moe>
2024-04-14 01:17:29 -0400

    Infini-SD: increase root size to 64gb

--- hosts/Infini-SD/filesystems.nix
+++ hosts/Infini-SD/filesystems.nix
@@ -17,7 +17,7 @@ in
     "/" = {
       device = "none";
       fsType = "tmpfs";
-      options = [ "defaults" "size=4G" "mode=755" ];
+      options = [ "defaults" "size=64G" "mode=755" ];
     };
 
     "/media/main" = mkMain' [ ];
