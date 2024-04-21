f0d148ea75428bc4bf56b909df95891219801625
Infinidoge <infinidoge@inx.moe>
2024-04-21 18:59:33 -0400

    users/root: remove unnecessary ssh match block

--- users/root/default.nix
+++ users/root/default.nix
@@ -3,12 +3,4 @@
     hashedPasswordFile = lib.mkIf config.modules.secrets.enable config.secrets.root-password;
     openssh.authorizedKeys.keys = import ./ssh-keys.nix;
   };
-
-  home-manager.users.root = { ... }: {
-    programs.ssh.matchBlocks = {
-      "server.doge-inc.net" = {
-        identityFile = "/root/.ssh/id_25519";
-      };
-    };
-  };
 }
