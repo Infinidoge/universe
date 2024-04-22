618db5ef65986fdf4f9f0c4db6e6bc17ac6e99d1
Infinidoge <infinidoge@inx.moe>
2024-04-22 14:18:57 -0400

    users/root: readd home manager block

--- users/root/default.nix
+++ users/root/default.nix
@@ -3,4 +3,6 @@
     hashedPasswordFile = lib.mkIf config.modules.secrets.enable config.secrets.root-password;
     openssh.authorizedKeys.keys = import ./ssh-keys.nix;
   };
+
+  home-manager.users.root = { ... }: {};
 }
