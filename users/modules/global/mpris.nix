a14319f43ca1160fa41ed3c0cddcb056c2cdc653
Infinidoge <infinidoge@inx.moe>
2024-04-28 01:18:48 -0400

    global/mpris: remove mpd, rename module to mpris

--- users/modules/global/mpris.nix
+++ users/modules/global/mpris.nix
@@ -3,16 +3,6 @@
 {
   config = lib.mkIf main.info.graphical {
     services = {
-      mpd = {
-        enable = true;
-        extraConfig = ''
-          audio_output {
-            type "pipewire"
-            name "PipeWire Sound Server"
-          }
-        '';
-      };
-      mpd-mpris.enable = true;
       mpris-proxy.enable = true;
       playerctld.enable = true;
     };
