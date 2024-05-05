eaecb1f85b31b78c26c7a024e38a7954f26fc858
Infinidoge <infinidoge@inx.moe>
2024-05-05 04:57:59 -0400

    global/general: remove redundant nix-channel activation script

--- modules/global/general.nix
+++ modules/global/general.nix
@@ -24,12 +24,6 @@
 
   services.timesyncd.extraConfig = "FallbackNTP=162.159.200.1 2606:4700:f1::1"; # time.cloudflare.com
 
-  system.activationScripts = {
-    # FIX: command-not-found database doesn't exist normally
-    # FIXME: activation script doesn't work properly, and frequently errors
-    # channels-update.text = "${pkgs.nix}/bin/nix-channel --update";
-  };
-
   # Reenable when CI and a binary cache is setup
   #environment.noXlibs = lib.mkDefault (!config.info.graphical);
 
