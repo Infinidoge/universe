adae967671457b8f74b994714228c5f7c75fe255
Infinidoge <infinidoge@inx.moe>
2024-05-08 03:40:28 -0400

    global/security: add common email/domain configuration

--- modules/global/security.nix
+++ modules/global/security.nix
@@ -84,7 +84,10 @@ with lib;
     };
   };
 
-  common = {
+  common = rec {
+    domain = "inx.moe";
+    subdomain = subdomain: "${subdomain}.${domain}";
+
     nginx = rec {
       ssl-cert = {
         enableACME = true;
@@ -102,6 +105,25 @@ with lib;
       account = "de3482";
       user = "${account}s1";
       host = "${account}.rsync.net";
+    };
+
+    email = rec {
+      withUser = user: "${user}@${domain}";
+      outgoingUser = "noreply";
+      incomingUser = "incoming";
+      outgoing = withUser outgoingUser;
+      incoming = withUser incomingUser;
+      withSubaddress = subaddress: "${outgoingUser}+${subaddress}@${domain}";
+
+      smtp = {
+        address = "smtp.purelymail.com";
+        SSLTLS = 465;
+        STARTTLS = 587;
+      };
+      imap = {
+        address = "imap.purelymail.com";
+        port = 993;
+      };
     };
   };
 }
