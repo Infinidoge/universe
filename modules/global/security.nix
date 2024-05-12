d7ce520c563fcf560775aa74d0f5aeb57144f99f
Infinidoge <infinidoge@inx.moe>
2024-05-12 00:02:54 -0400

    global/security: move common config to global/common

--- modules/global/security.nix
+++ modules/global/security.nix
@@ -60,47 +60,4 @@ with lib;
 
   # For permission to access smtp password
   users.groups.smtp = { };
-
-  common = rec {
-    domain = "inx.moe";
-    subdomain = subdomain: "${subdomain}.${domain}";
-
-    nginx = rec {
-      ssl-cert = {
-        enableACME = true;
-        acmeRoot = null;
-      };
-      ssl-optional = ssl-cert // {
-        addSSL = true;
-      };
-      ssl = ssl-cert // {
-        forceSSL = true;
-      };
-    };
-
-    rsyncnet = rec {
-      account = "de3482";
-      user = "${account}s1";
-      host = "${account}.rsync.net";
-    };
-
-    email = rec {
-      withUser = user: "${user}@${domain}";
-      outgoingUser = "noreply";
-      incomingUser = "incoming";
-      outgoing = withUser outgoingUser;
-      incoming = withUser incomingUser;
-      withSubaddress = subaddress: "${outgoingUser}+${subaddress}@${domain}";
-
-      smtp = {
-        address = "smtp.purelymail.com";
-        SSLTLS = 465;
-        STARTTLS = 587;
-      };
-      imap = {
-        address = "imap.purelymail.com";
-        port = 993;
-      };
-    };
-  };
 }
