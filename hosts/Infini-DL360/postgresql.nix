01640b7232c78ff3f7b2fd0c284078248069dd8a
Infinidoge <infinidoge@inx.moe>
2024-05-19 05:16:54 -0400

    Infini-DL360/postgresql: init

---
+++ hosts/Infini-DL360/postgresql.nix
@@ -0,0 +1,13 @@
+{ pkgs, config, lib, ... }:
+let
+  directory = "/srv/postgresql";
+in
+{
+  persist.directories = [ { inherit directory; user = "postgres"; group = "postgresl"; } ];
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_16;
+    dataDir = "${directory}/${config.services.postgresql.package.psqlSchema}";
+  };
+}
