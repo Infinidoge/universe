c1c2364796b4bfc5ac63b793992c5bf9fb8ba8af
Infinidoge <infinidoge@inx.moe>
2024-05-20 13:21:32 -0400

    pkgs/bytecode-viewer: update mvnHash

--- pkgs/bytecode-viewer.nix
+++ pkgs/bytecode-viewer.nix
@@ -17,7 +17,7 @@ maven.buildMavenPackage rec {
     sha256 = "sha256-opAUmkEcWPOrcxAL+I1rBQXwHmvzbu0+InTnsg9r+z8=";
   };
 
-  mvnHash = "sha256-gmERhg7SJACJk2L/l8fF1LkG7q4WAJOJA8qnHMV5Ubc=";
+  mvnHash = "sha256-mBTYQuybcFIm1KXYDXAk0+m5qEtmRQT7s/XsS50cV8s=";
 
   nativeBuildInputs = [ makeWrapper ];
 
