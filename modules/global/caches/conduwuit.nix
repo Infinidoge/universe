18d789747cd45a3b28d59d6d2207880a725c061e
Infinidoge <infinidoge@inx.moe>
2024-06-08 20:53:47 -0400

    caches/conduwuit: update public key

--- modules/global/caches/conduwuit.nix
+++ modules/global/caches/conduwuit.nix
@@ -4,7 +4,7 @@
       "https://attic.kennel.juneis.dog/conduwuit"
     ];
     trusted-public-keys = [
-      "conduwuit:lYPVh7o1hLu1idH4Xt2QHaRa49WRGSAqzcfFd94aOTw="
+      "conduwuit:BbycGUgTISsltcmH0qNjFR9dbrQNYgdIAcmViSGoVTE="
     ];
   };
 }
