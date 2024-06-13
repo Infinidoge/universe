88821ac7975b7af238e0e515c00fb43da9d0c6e1
Infinidoge <infinidoge@inx.moe>
2024-06-13 08:59:39 -0400

    config/doom: unpin old packages

--- users/infinidoge/config/doom/packages.el
+++ users/infinidoge/config/doom/packages.el
@@ -51,6 +51,3 @@
 
 (package! emacsql-sqlite :built-in 'prefer)
 (package! pdf-tools :built-in 'prefer)
-
-(package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
-(package! with-editor :pin "391e76a256aeec6b9e4cbd733088f30c677d965b")
