667b8df297d43cdd174ba8c8fad119cc6af2bf58
Infinidoge <infinidoge@inx.moe>
2024-07-05 22:00:56 -0400

    global/obs-studio: init

---
+++ users/modules/global/obs-studio.nix
@@ -0,0 +1,10 @@
+{ pkgs, main, config, ... }:
+
+{
+  programs.obs-studio = {
+    enable = main.info.graphical;
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-pipewire-audio-capture
+    ];
+  };
+}
