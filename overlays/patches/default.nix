a14bc7ca835bdaf5bc1386141493965531ad760a
Infinidoge <infinidoge@inx.moe>
2024-07-13 21:03:28 -0400

    patches: remove broken fprintd patch

--- overlays/patches/default.nix
+++ overlays/patches/default.nix
@@ -13,12 +13,12 @@ in
 
   tailscale-doge = addPatches prev.tailscale [ ./tailscale-cgnat.patch ];
 
-  fprintd = addPatches prev.fprintd [
-    (final.fetchpatch {
-      url = "https://gitlab.freedesktop.org/libfprint/fprintd/-/commit/8a8162daa2fb08210ecc4c48d1f10ee97cc7a088.patch";
-      hash = "sha256-05tNiv2wdztkjBBiBS5hpuC5n3QrsJ5o15Uv7Q42on4=";
-    })
-  ];
+  #fprintd = addPatches prev.fprintd [
+  #  (final.fetchpatch {
+  #    url = "https://gitlab.freedesktop.org/libfprint/fprintd/-/commit/8a8162daa2fb08210ecc4c48d1f10ee97cc7a088.patch";
+  #    hash = "sha256-05tNiv2wdztkjBBiBS5hpuC5n3QrsJ5o15Uv7Q42on4=";
+  #  })
+  #];
 
   hyfetch = addPatches prev.hyfetch [ ./neofetch-remove-steam-packages.patch ];
 
