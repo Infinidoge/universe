421d490af161ce57ad0e647320766118272d9f24
Infinidoge <infinidoge@inx.moe>
2024-07-26 10:49:33 -0400

    global/locale: init, refactored from modules/locale

---
+++ modules/global/locale.nix
@@ -0,0 +1,23 @@
+{ common, ... }:
+let
+  cfg = common.locale;
+in
+{
+  common.locale = {
+    keymap = "us";
+    locale = "en_US.UTF-8";
+    timezone = "America/New_York";
+  };
+
+  console.keyMap = cfg.keymap;
+  services.xserver.xkb = {
+    layout = cfg.keymap;
+    options = "compose:ralt";
+  };
+
+  services.libinput.enable = true;
+
+  i18n.defaultLocale = cfg.locale;
+
+  time.timeZone = cfg.timezone;
+}
