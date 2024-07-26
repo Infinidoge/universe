c795ef4741a307e3cc4d5c59dca615814e9e54e9
Infinidoge <infinidoge@inx.moe>
2024-07-26 10:53:55 -0400

    Infini-FRAMEWORK: enable debug info

--- hosts/Infini-FRAMEWORK/default.nix
+++ hosts/Infini-FRAMEWORK/default.nix
@@ -19,6 +19,8 @@
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   nix.settings.extra-platforms = config.boot.binfmt.emulatedSystems;
 
+  environment.enableDebugInfo = true;
+
   modules = {
     hardware = {
       gpu.intel = true;
