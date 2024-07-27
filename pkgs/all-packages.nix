02784d760c5fc61b2f21e14f7387eb89b1e1bdff
Infinidoge <infinidoge@inx.moe>
2024-07-27 00:12:57 -0400

    pkgs/mkSymlinks: remove

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -4,7 +4,6 @@
   fw-ectool = pkgs.callPackage ./fw-ectool.nix { };
   hexagon = pkgs.callPackage ./hexagon.nix { };
   mcaselector = pkgs.callPackage ./mcaselector.nix { };
-  mkSymlinks = pkgs.callPackage ./mk-symlinks.nix { };
   neocities = pkgs.callPackage ./neocities { };
   nix-modrinth-prefetch = pkgs.callPackage ./nix-modrinth-prefetch.nix { };
   olympus = pkgs.callPackage ./olympus.nix { };
