c6a66bd42d1a12349d1302265b8921cc07f1900b
Infinidoge <infinidoge@inx.moe>
2024-08-17 03:55:01 -0400

    config/doom: remove hydra and rgb modules

--- users/infinidoge/config/doom/init.el
+++ users/infinidoge/config/doom/init.el
@@ -29,7 +29,6 @@
  doom-quit                      ; DOOM quit-message prompts when you quit Emacs
  ;;(emoji +unicode)             ; 🙂
  hl-todo                        ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
- hydra
  indent-guides                  ; highlighted indent columns
  ligatures                      ; ligatures and symbols to make your code pretty again
  ;;minimap                      ; show a map of the code on the side
@@ -98,7 +97,6 @@
  ;; (pass +auth)                ; password manager for nerds
  pdf                            ; pdf enhancements
  ;;prodigy                      ; FIXME managing external services & code builders
- rgb                            ; creating color strings
  ;;taskrunner                   ; taskrunner for all your projects
  ;;terraform                    ; infrastructure as code
  ;;tmux                         ; an API for interacting with tmux
