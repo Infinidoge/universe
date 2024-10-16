cbb614659bbcc8b2989f21be21eaaab64ff22fe7
Infinidoge <infinidoge@inx.moe>
2024-10-16 02:01:00 -0400

    users/infinidoge: update uwufetch url

--- users/infinidoge/config/default.nix
+++ users/infinidoge/config/default.nix
@@ -14,7 +14,7 @@ with lib;
       src = ./neofetch.conf;
 
       image = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/TheDarkBug/uwufetch/main/res/nixos.png";
+        url = "https://raw.githubusercontent.com/ad-oliviero/uwufetch/main/res/nixos.png";
         sha256 = "007q947q2a5c8z9r6cc6mj3idq0ss9zsi9xvij8l8chkjnh8fwn2";
       };
       inherit (main.info) model;
