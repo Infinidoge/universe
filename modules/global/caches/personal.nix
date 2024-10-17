fd15960aab3a0871135d69329a5cd3f83e37e3d1
Infinidoge <infinidoge@inx.moe>
2024-10-17 11:39:51 -0400

    caches/personal: set higher priority

--- modules/global/caches/personal.nix
+++ modules/global/caches/personal.nix
@@ -2,8 +2,8 @@
 {
   nix.settings = {
     substituters = lib.flatten [
-      (lib.optional (config.networking.hostName != "Infini-DESKTOP" && config.info.loc.home) "ssh://infini-desktop")
-      "https://hydra.inx.moe"
+      (lib.optional (config.networking.hostName != "Infini-DESKTOP" && config.info.loc.home) "ssh://infini-desktop?priority=9")
+      "https://hydra.inx.moe?priority=10"
     ];
     trusted-public-keys = [
       "infinidoge-1:uw2A6JHHdGJ9GPk0NEDnrdfVkPp0CUY3zIvwVgNlrSk="
