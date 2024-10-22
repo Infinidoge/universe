e722d72afbd665a06260253e9071ea7a54ab4dfa
Infinidoge <infinidoge@inx.moe>
2024-10-22 12:22:44 -0400

    Infini-DL360/web: add swedish.fish and sweedish.fish

--- hosts/Infini-DL360/web.nix
+++ hosts/Infini-DL360/web.nix
@@ -32,6 +32,7 @@ let
 
   redirects = lib.mapAttrs mkRedirect {
     "nitter.inx.moe" = "twitter.com";
+    "sweedish.fish" = "swedish.fish";
   };
 in
 {
@@ -50,6 +51,15 @@ in
       };
     };
     "blahaj.inx.moe" = ssl-optional // {
+      locations."/" = {
+        tryFiles = "/Blahaj.png =404";
+        root = ./static;
+      };
+      locations."/buy" = {
+        return = "301 https://www.ikea.com/us/en/p/blahaj-soft-toy-shark-90373590/";
+      };
+    };
+    "swedish.fish" = ssl-optional // {
       locations."/" = {
         tryFiles = "/Blahaj.png =404";
         root = ./static;
