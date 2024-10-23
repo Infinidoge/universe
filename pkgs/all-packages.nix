542da6d33b168a6f855ea4a7cc1d03e31f0c48f0
Infinidoge <infinidoge@inx.moe>
2024-10-23 01:43:45 -0400

    pkgs: sort all-packages

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -1,16 +1,16 @@
 { pkgs }:
 {
+  bytecode-viewer = pkgs.callPackage ./bytecode-viewer.nix { };
   ears-cli = pkgs.callPackage ./ears-cli.nix { };
   fw-ectool = pkgs.callPackage ./fw-ectool.nix { };
   hexagon = pkgs.callPackage ./hexagon.nix { };
+  jupyterlab-vim = pkgs.python3Packages.callPackage ./jupyterlab-vim { };
   mcaselector = pkgs.callPackage ./mcaselector.nix { };
   nix-modrinth-prefetch = pkgs.callPackage ./nix-modrinth-prefetch.nix { };
   olympus = pkgs.callPackage ./olympus.nix { };
   sim65 = pkgs.callPackage ./sim65.nix { };
   substituteSubset = pkgs.callPackage ./substitute-subset.nix { };
-  unbted = pkgs.callPackage ./unbted.nix { };
-  bytecode-viewer = pkgs.callPackage ./bytecode-viewer.nix { };
-  unmap = pkgs.callPackage ./unmap { };
   tmx-cups-ppd = pkgs.callPackage ./tmx-cups-ppd.nix { };
-  jupyterlab-vim = pkgs.python3Packages.callPackage ./jupyterlab-vim { };
+  unbted = pkgs.callPackage ./unbted.nix { };
+  unmap = pkgs.callPackage ./unmap { };
 }
