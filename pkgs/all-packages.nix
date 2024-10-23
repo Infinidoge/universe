43b393399e5464853f2cd9016d4f37bd4c98c2c5
Infinidoge <infinidoge@inx.moe>
2024-10-23 02:56:57 -0400

    pkgs/jupyterlab-myst: init

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -4,6 +4,7 @@
   ears-cli = pkgs.callPackage ./ears-cli.nix { };
   fw-ectool = pkgs.callPackage ./fw-ectool.nix { };
   hexagon = pkgs.callPackage ./hexagon.nix { };
+  jupyterlab-myst = pkgs.python3Packages.callPackage ./jupyterlab-myst { };
   jupyterlab-vim = pkgs.python3Packages.callPackage ./jupyterlab-vim { };
   mcaselector = pkgs.callPackage ./mcaselector.nix { };
   nix-modrinth-prefetch = pkgs.callPackage ./nix-modrinth-prefetch.nix { };
