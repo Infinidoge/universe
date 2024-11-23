ae24df0f981fed57c823d2472cbbe3d581c489f4
Infinidoge <infinidoge@inx.moe>
2024-11-23 04:06:04 -0500

    Infini-STICK: resetup with disko

--- hosts/Infini-STICK/install.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# [[file:readme.org::install][install]]
-# [[file:readme.org::install][installing]]
-echo "LOG: Installing NixOS"
-sudo nixos-install --flake /etc/nixos#Infini-STICK --no-root-password
-# installing ends here
-# install ends here
