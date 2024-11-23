3e0eee4082c696f34d9dd5d3ec6de204f90f8c5c
Infinidoge <infinidoge@inx.moe>
2024-11-23 09:42:19 -0500

    hestia: init

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -15,6 +15,7 @@ let
     Infini-SD = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8oViHNz64NG51uyll/q/hrSGwoHRgvYI3luD/IWTUT root@Infini-SD";
     Infini-DL360 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPjmvE76BcPwZSjeNGzlguDQC67Yxa3uyOf5ZmVDWNys root@Infini-DL360";
     Infini-RASPBERRY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIwPqTFCztLbYFFUej42hRzzCBzG6BCZIb7zXi2cxeJp root@Infini-RASPBERRY";
+    hestia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIUMLnWIbl6WNZxm22uyDv5mM27hPCn8u8ZZ8EpF+O/3 root@hestia";
   };
   users = {
     infinidoge = import ../users/infinidoge/ssh-keys.nix;
