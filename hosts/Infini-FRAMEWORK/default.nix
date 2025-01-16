f389574f0f14d3ae81388d096849e727377d7363
Infinidoge <infinidoge@inx.moe>
2025-01-16 14:09:57 -0500

    Infini-FRAMEWORK: add remote receipt printer

--- hosts/Infini-FRAMEWORK/default.nix
+++ hosts/Infini-FRAMEWORK/default.nix
@@ -59,6 +59,11 @@
       deviceUri = "usb://EPSON/TM-m30II-NT?serial=5839394D0032780000";
       model = "tm-m30-rastertotmt.ppd.gz";
     }
+    {
+      name = "EPSON-TM-m30-remote";
+      deviceUri = "ipp://100.101.102.18/printers/EPSON-TM-m30";
+      model = "raw";
+    }
   ];
 
   services.fwupd = {
