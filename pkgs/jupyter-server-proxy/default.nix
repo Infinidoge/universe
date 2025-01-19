7278dc04bc8a3e00898a2577f04c886085693411
Infinidoge <infinidoge@inx.moe>
2025-01-19 13:56:07 -0500

    pkgs/vpython-jupyter: init with dependencies

---
+++ pkgs/jupyter-server-proxy/default.nix
@@ -0,0 +1,68 @@
+{ buildPythonPackage
+, fetchFromGitHub
+
+, yarnConfigHook
+, fetchYarnDeps
+, hatchling
+, hatch-jupyter-builder
+, nodejs
+
+, jupyterlab
+, aiohttp
+, importlib-metadata
+, jupyter-server
+, simpervisor
+, tornado
+, traitlets
+}:
+
+buildPythonPackage {
+  pname = "jupyter-server-proxy";
+  version = "4.4.1-0.dev";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "jupyterhub";
+    repo = "jupyter-server-proxy";
+    rev = "44db8eec5b0fb7f4f842f3f30b9c17b5ed2ceec5";
+    hash = "sha256-AQr14CK/aT5rB46jmGzEATI6R/QoMt7RKUBSnoQuDzE=";
+  };
+
+  yarnOfflineCache = fetchYarnDeps {
+    yarnLock = ./yarn.lock;
+    hash = "sha256-9zErE3SFrt7O7nKzRSX5Lup3z7b1c8GSCAMpF7hkiOU=";
+  };
+
+  nativeBuildInputs = [
+    yarnConfigHook
+    nodejs
+  ];
+
+  build-system = [
+    hatchling
+    hatch-jupyter-builder
+    jupyterlab
+  ];
+
+  dependencies = [
+    aiohttp
+    importlib-metadata
+    jupyter-server
+    simpervisor
+    tornado
+    traitlets
+  ];
+
+  preConfigure = ''
+    substituteInPlace labextension/package.json \
+        --replace-fail 'jlpm' 'yarn'
+    pushd labextension
+    cp ${./yarn.lock} yarn.lock
+  '';
+
+  preBuild = ''
+    # Generate the jupyterlab extension files
+    yarn --offline run build:prod
+    popd
+  '';
+}
---
+++ pkgs/simpervisor.nix
@@ -0,0 +1,18 @@
+{ buildPythonPackage, fetchFromGitHub, hatchling }:
+
+buildPythonPackage {
+  pname = "simpervisor";
+  version = "0-unstable-2024-03-07";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "jupyterhub";
+    repo = "simpervisor";
+    rev = "f29189ec6eb961caa5b26782cac1645db074ddce";
+    hash = "sha256-rmEQ68Ll1cIzQrYXHTkLYVxog3kPkieExJ1xBTeQ2nc=";
+  };
+
+  build-system = [
+    hatchling
+  ];
+}
