ec1b93a890f81111cd61f4db88699c2b372ea2e8
Infinidoge <infinidoge@inx.moe>
2025-01-19 14:28:48 -0500

    glonal/keychain: fix ssh agent and allow inheritance

--- users/modules/global/keychain.nix
+++ users/modules/global/keychain.nix
@@ -1,9 +1,11 @@
 { config, ... }:
 {
+  services.ssh-agent.enable = true;
+
   programs.keychain = {
     enable = true;
+    inheritType = "any";
     extraFlags = [ "--quiet" "--dir ${config.xdg.configHome}/keychain" ];
-
     keys = [ "id_ed25519" ];
   };
 }
