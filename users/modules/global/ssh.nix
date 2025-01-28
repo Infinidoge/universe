da88ac19af153e80a9e37ddf57b3a564cc756d5f
Infinidoge <infinidoge@inx.moe>
2025-01-28 15:14:29 -0500

    global/ssh: remove unused match block

--- users/modules/global/ssh.nix
+++ users/modules/global/ssh.nix
@@ -5,11 +5,6 @@
     addKeysToAgent = "yes";
     controlMaster = "auto";
     controlPersist = "1m";
-    matchBlocks = {
-      "server.doge-inc.net" = {
-        port = 245;
-      };
-    };
   };
 
   services.ssh-agent.enable = true;
