f37e9002d8cb2b0e112c640da3a4b9759be59711
Infinidoge <infinidoge@inx.moe>
2025-02-15 04:12:10 -0500

    Infini-OPTIPLEX: setup remote printing

--- hosts/Infini-OPTIPLEX/default.nix
+++ hosts/Infini-OPTIPLEX/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -14,9 +14,29 @@
   modules = {
     hardware.form.desktop = true;
     hardware.gpu.intel = true;
+    hardware.peripherals.printing.enable = true;
     desktop.wm.enable = true;
   };
 
+  services.printing = {
+    listenAddresses = [
+      "localhost:631"
+      "100.101.102.18:631"
+      "infini-optiplex:631"
+    ];
+    allowFrom = [ "all" ];
+    defaultShared = true;
+    openFirewall = true;
+    drivers = [ pkgs.tmx-cups-ppd ];
+  };
+
+  hardware.printers.ensurePrinters = [
+    {
+      name = "EPSON-TM-m30";
+      deviceUri = "usb://EPSON/TM-m30II-NT?serial=5839394D0032780000";
+      model = "tm-m30-rastertotmt.ppd.gz";
+    }
+  ];
 
   persist = {
     directories = [
