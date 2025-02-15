bcb68507325b84d7a69790cb85501eecf993f07a
Infinidoge <infinidoge@inx.moe>
2025-02-15 04:04:53 -0500

    hosts/*: remove unused org install scripts

--- hosts/Infini-DESKTOP/bare_install.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# [[file:readme.org::bare_install][bare_install]]
-# [[file:readme.org::bare_install][installing]]
-echo "LOG: Installing NixOS"
-sudo nixos-install --flake /etc/nixos#Infini-DESKTOP --no-root-password
-# installing ends here
-# bare_install ends here
