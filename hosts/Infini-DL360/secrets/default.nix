aab00060557c2f7c6023ba2dbcf0e7411764813c
Infinidoge <infinidoge@inx.moe>
2025-02-18 21:54:34 -0500

    Infini-DL360/secrets: only use hydra group where necessary

--- hosts/Infini-DL360/secrets/default.nix
+++ hosts/Infini-DL360/secrets/default.nix
@@ -6,6 +6,8 @@ in
   age.secrets = {
     authentik-ldap.rekeyFile = ./authentik-ldap.age;
     authentik.rekeyFile = ./authentik.age;
+    binary-cache-private-key.group = "hydra";
+    binary-cache-private-key.mode = "440";
     freshrss = withOwnerGroup "freshrss" ./freshrss.age;
     hedgedoc = withOwnerGroup "hedgedoc" ./hedgedoc.age;
     hydra = withGroup "hydra" ./hydra.age;
--- secrets/default.nix
+++ secrets/default.nix
@@ -21,7 +21,7 @@ in
     age.secrets = {
       borg-ssh-key.rekeyFile = ./borg-ssh-key.age;
       borg-password = withGroup "borg" ./borg-password.age;
-      binary-cache-private-key = withGroup "hydra" ./binary-cache-private-key.age;
+      binary-cache-private-key.rekeyFile = ./binary-cache-private-key.age;
       smtp-noreply = withGroup "smtp" ./smtp-noreply.age;
       dns-cloudflare.rekeyFile = ./dns-cloudflare.age;
     };
