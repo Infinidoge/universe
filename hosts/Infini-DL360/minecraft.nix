a4fbb91532ac542bd0b7a0c5171d25f34c758b12
Infinidoge <infinidoge@inx.moe>
2025-05-08 15:39:32 -0400

    Infini-DL360/minecraft: migrate server properties and jvm opts

--- hosts/Infini-DL360/minecraft.nix
+++ hosts/Infini-DL360/minecraft.nix
@@ -3,11 +3,28 @@
 {
   services.minecraft-servers.servers.hackcraft = {
     enable = true;
+    jvmOpts = [
+      "-Xmx8G"
+      "-Xms8G"
+
+      "-XX:+UseZGC"
+      "-XX:+ZGenerational"
+      "-XX:+UseNUMA"
+      "-javaagent:unsup-1.1-beta1.jar"
+    ];
+    serverProperties = {
+      motd = "Hacking, and perhaps, even crafting!";
+      difficulty = "normal";
+      allow-flight = true;
+      enforce-secure-profile = false;
+      server-port = 25675;
+      spawn-protection = 0;
+    };
     package = pkgs.writeShellApplication {
       name = "mincraft-server";
       runtimeInputs = with pkgs; [ openjdk21 ];
       text = ''
-        java @user_jvm_args.txt "$@" @libraries/net/minecraftforge/forge/1.20.1-47.4.0/unix_args.txt nogui
+        java "$@" @libraries/net/minecraftforge/forge/1.20.1-47.4.0/unix_args.txt nogui
       '';
     };
   };
