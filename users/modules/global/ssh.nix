cb6906305a9647064a367f1edda590ff4122b973
Infinidoge <infinidoge@inx.moe>
2025-10-13 19:22:14 -0400

    global/ssh: move config to match block

--- users/modules/global/ssh.nix
+++ users/modules/global/ssh.nix
@@ -2,9 +2,12 @@
 {
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
-    controlMaster = "auto";
-    controlPersist = "1m";
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      addKeysToAgent = "yes";
+      controlMaster = "auto";
+      controlPersist = "1m";
+    };
   };
 
   services.ssh-agent.enable = true;
