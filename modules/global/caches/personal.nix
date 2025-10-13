abafda7f5c5ba9e6fea478114729cb6ad0a50e8a
Infinidoge <infinidoge@inx.moe>
2025-10-13 19:46:14 -0400

    caches/personal: clean up, don't refer to cache hosted on self

--- modules/global/caches/personal.nix
+++ modules/global/caches/personal.nix
@@ -1,11 +1,14 @@
 { config, lib, ... }:
+let
+  inherit (lib) optional;
+  inherit (config.networking) hostName;
+  inherit (config.info.loc) home;
+in
 {
   nix.settings = {
     substituters = lib.flatten [
-      (lib.optional (
-        config.networking.hostName != "Infini-DESKTOP" && config.info.loc.home
-      ) "ssh-ng://infini-desktop?priority=9")
-      "https://hydra.inx.moe?priority=10"
+      (optional (hostName != "Infini-DESKTOP" && home) "ssh-ng://infini-desktop?priority=9")
+      (optional (hostName != "Infini-DL360") "https://hydra.inx.moe?priority=10")
     ];
     trusted-public-keys = [
       "infinidoge-1:uw2A6JHHdGJ9GPk0NEDnrdfVkPp0CUY3zIvwVgNlrSk="
