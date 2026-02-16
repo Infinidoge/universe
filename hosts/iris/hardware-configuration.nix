c98afcf742074cd404ce6adc51c9d4e6b60fdbdb
Infinidoge <infinidoge@inx.moe>
2026-02-16 01:52:12 -0500

    iris: add hardware configuration

---
+++ hosts/iris/hardware-configuration.nix
@@ -0,0 +1,25 @@
+{
+  lib,
+  modulesPath,
+  ...
+}:
+
+{
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [
+    "ahci"
+    "virtio_pci"
+    "virtio_scsi"
+    "xhci_pci"
+    "sr_mod"
+    "virtio_blk"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
