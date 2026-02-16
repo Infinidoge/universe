c116f915c3524223d4941731d5f08128b4b92fb6
Infinidoge <infinidoge@inx.moe>
2026-02-16 02:40:11 -0500

    hestia: remove unnecessary bits

--- hosts/hestia/default.nix
+++ hosts/hestia/default.nix
@@ -38,13 +38,5 @@
 
   age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBanlhzmtBf5stg2yYdxqb9FzFZmum/rlWod/akWQI3c root@hestia";
 
-  modules.hardware.form.server = true;
-  modules.backups.enable = false; # hestia is a backup target
   boot.loader.timeout = 1;
-
-  swapDevices = lib.mkForce [
-    {
-      device = "/dev/disk/by-uuid/a41fad08-24ee-421b-a2c5-59571bdc0c80";
-    }
-  ];
 }
--- hosts/hestia/disks.nix
+++ hosts/hestia/disks.nix
@@ -44,6 +44,5 @@ with lib.our.disko;
     "/persist"
     "/storage"
     "/etc/ssh"
-    "/home"
   ];
 }
