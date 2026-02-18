db826e66447539449facc86c3cf61c6bfff60804
Infinidoge <infinidoge@inx.moe>
2026-02-17 21:21:40 -0500

    nixos/rsyncnet: pre-include rsync.net host key

--- nixos/rsyncnet.nix
+++ nixos/rsyncnet.nix
@@ -15,4 +15,7 @@
         Hostname ${host}
         User ${account}
   '';
+
+  services.openssh.knownHosts.${common.rsyncnet.host}.publicKey =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIObQN4P/deJ/k4P4kXh6a9K4Q89qdyywYetp9h3nwfPo";
 }
