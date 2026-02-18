fd61bae30ea3df60c311681d90ab6851660717f2
Infinidoge <infinidoge@inx.moe>
2026-02-18 00:41:54 -0500

    iris/dns: add allowed secondaries

--- hosts/iris/dns.nix
+++ hosts/iris/dns.nix
@@ -5,6 +5,10 @@
       "inx.moe" = {
         master = true;
         file = "/srv/dns/inx.moe";
+        slaves = [
+          "216.218.133.2" # slave.dns.he.net
+          "128.210.6.103" # daedalus
+        ];
       };
     };
   };
