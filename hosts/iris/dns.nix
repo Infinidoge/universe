b0fc4c98c05bf56825be8dd219535a7555ca1b07
Infinidoge <infinidoge@inx.moe>
2026-02-19 03:17:55 -0500

    iris/dns: add other domains

--- hosts/iris/dns.nix
+++ hosts/iris/dns.nix
@@ -38,6 +38,12 @@ in
     zones = {
       "inx.moe" = mkPrimaryZone "/srv/dns/inx.moe";
       "challenge.inx.moe" = mkZone "/srv/dns/challenge.inx.moe" { };
+
+      "doge-inc.net" = mkPrimaryZone "/srv/dns/doge-inc.net";
+      "foxy.software" = mkPrimaryZone "/srv/dns/foxy.software";
+      "swedish.fish" = mkPrimaryZone "/srv/dns/swedish.fish";
+      "unreliable.email" = mkPrimaryZone "/srv/dns/unreliable.email";
+      "vulcan.moe" = mkPrimaryZone "/srv/dns/vulcan.moe";
     };
   };
 }
