fd0d4c7e1e462335b19bfb1a0b81a34fa9a24c60
Infinidoge <infinidoge@inx.moe>
2026-03-01 20:50:00 -0500

    nixos/kmscon: remove unnecessary rec

--- nixos/kmscon.nix
+++ nixos/kmscon.nix
@@ -5,10 +5,10 @@
     hwRender = false;
     useXkbConfig = true;
     fonts = [
-      (rec {
+      {
         name = "DejaVuSansMono";
         package = pkgs.nerd-fonts.dejavu-sans-mono;
-      })
+      }
     ];
     extraConfig = ''
       font-size=14
