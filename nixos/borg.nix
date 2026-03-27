d5b8f0f34c9fb72fda6ae1fe2f753d603fef3856
Infinidoge <infinidoge@inx.moe>
2026-03-26 21:40:02 -0400

    nixos/borg: use latest borg serve

--- nixos/borg.nix
+++ nixos/borg.nix
@@ -14,7 +14,7 @@ in
     # Borg Backup public key:
     # ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINhldH579ixPRSBtTjnzWoDCNyUxUSl1BjogWN3keYBR borg@universe
     BORG_RSH = "ssh -i ${secrets.borg-ssh-key}"; # private key for automated backups
-    BORG_REMOTE_PATH = "/usr/local/bin/borg1/borg1";
+    BORG_REMOTE_PATH = "/usr/local/bin/borg14/borg14";
     BORG_PASSCOMMAND = "cat ${secrets.borg-password}";
     BORG_REPO = "rsync.net:backups/hosts";
   };
