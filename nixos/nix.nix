cd46dd7b73b42dd5365a477bafa45a92117814e2
Infinidoge <infinidoge@inx.moe>
2026-04-18 07:44:59 -0400

    nixos/nix: add cache.nixos.org key
    Just in case

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -213,6 +213,7 @@ in
       "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "infinidoge-1:uw2A6JHHdGJ9GPk0NEDnrdfVkPp0CUY3zIvwVgNlrSk="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
