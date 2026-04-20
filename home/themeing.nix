e3f78ce7b03a66be96b708ff4bc72d6e92a7e130
Infinidoge <infinidoge@inx.moe>
2026-04-19 23:08:08 -0400

    home/themeing: set gtk4 theme to gtk theme

--- home/themeing.nix
+++ home/themeing.nix
@@ -7,6 +7,7 @@
   gtk = {
     enable = true;
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+    gtk4.theme = config.gtk.theme;
 
     font = {
       package = pkgs.dejavu_fonts;
