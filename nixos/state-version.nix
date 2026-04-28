240aab1813488c8cad6af51e7cd913cc29d3c3d2
Infinidoge <infinidoge@inx.moe>
2026-04-28 13:58:28 -0400

    nixos/state-version: change logRefusedConnections default

--- nixos/state-version.nix
+++ nixos/state-version.nix
@@ -6,4 +6,7 @@ in
 {
   # Defaults to `true` for stateVersion < 23.11
   boot.swraid.enable = mkDefault false;
+
+  # Defaults to `true` for stateVersion < 26.05
+  networking.firewall.logRefusedConnections = mkDefault false;
 }
