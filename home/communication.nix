d66946a1f6e05a144155c2c86b5df84802c4092f
Infinidoge <infinidoge@inx.moe>
2026-04-29 22:04:21 -0400

    home/communication: allow insecure openssl

--- home/communication.nix
+++ home/communication.nix
@@ -1,8 +1,18 @@
 { pkgs, ... }:
+let
+  # BUG: openssl 1.1.1w is insecure
+  #      Override it to mark it as secure for just Discord
+  openssl_1_1 = pkgs.openssl_1_1.overrideAttrs (old: {
+    meta = old.meta // {
+      knownVulnerabilities = [ ];
+    };
+  });
+in
 {
   home.packages = with pkgs; [
     (discord-canary.override {
       inherit (pkgs) vencord;
+      inherit openssl_1_1;
       withVencord = true;
       withOpenASAR = true;
       withTTS = false;
@@ -10,6 +20,7 @@
     thunderbird
 
     (discord.override {
+      inherit openssl_1_1;
       withVencord = true;
       withOpenASAR = true;
       withTTS = false;
