b63b37ff787ae6bcd2e569e6764040e89bd79300
Infinidoge <infinidoge@inx.moe>
2026-05-12 22:34:00 -0400

    home/communication: drop openssl_1_1 hack

--- home/communication.nix
+++ home/communication.nix
@@ -1,18 +1,8 @@
 { pkgs, ... }:
-let
-  # BUG: openssl 1.1.1w is insecure
-  #      Override it to mark it as secure for just Discord
-  openssl_1_1 = pkgs.openssl_1_1.overrideAttrs (old: {
-    meta = old.meta // {
-      knownVulnerabilities = [ ];
-    };
-  });
-in
 {
   home.packages = with pkgs; [
     (discord-canary.override {
       inherit (pkgs) vencord;
-      inherit openssl_1_1;
       withVencord = true;
       withOpenASAR = true;
       withTTS = false;
@@ -20,7 +10,6 @@ in
     thunderbird
 
     (discord.override {
-      inherit openssl_1_1;
       withVencord = true;
       withOpenASAR = true;
       withTTS = false;
