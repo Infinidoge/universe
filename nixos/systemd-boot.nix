dc90b4d3410c0946eeb87af928811c3cea58fea2
Infinidoge <infinidoge@inx.moe>
2026-08-21 00:03:44 -0400

    nixos/systemd-boot: init

---
+++ nixos/systemd-boot.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      editor = false;
+    };
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
+  };
+}
