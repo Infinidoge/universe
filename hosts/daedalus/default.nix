54824dba32ab6f2086cd40c04bf5023f769515fd
Infinidoge <infinidoge@inx.moe>
2026-08-31 03:49:42 -0400

    daedalus: remove setup-infiniband service

--- hosts/daedalus/default.nix
+++ hosts/daedalus/default.nix
@@ -195,18 +195,6 @@
       '';
     };
 
-  systemd.services.setup-infiniband = {
-    wantedBy = [ "network.target" ];
-    script = ''
-      echo "eth" > /sys/bus/pci/devices/0000:04:00.0/mlx4_port1
-      echo "eth" > /sys/bus/pci/devices/0000:04:00.0/mlx4_port2
-    '';
-    serviceConfig = {
-      User = "root";
-      Group = "root";
-    };
-  };
-
   services.printing.enable = true;
 
   boot.supportedFilesystems.nfs = true;
