f677d76215ea31d159651773c95471f7cc14fc55
Infinidoge <infinidoge@inx.moe>
2026-08-31 03:17:04 -0400

    overrides: disable checks for failing hydra dependency

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -131,6 +131,32 @@ in
     doCheck = false;
   });
 
+  # BUG: OIDC-Lite fails to build
+  # Fix from: https://github.com/leo60228/dotfiles/blob/main/overlays/oidc-lite.nix
+  hydra = prev.hydra.overrideAttrs (
+    oldAttrs:
+    let
+      inherit (oldAttrs.passthru) perlDeps;
+      newPerlDeps = perlDeps.override {
+        paths = map (
+          x:
+          if x ? pname && x.pname == "OIDC-Lite" then
+            x.overrideAttrs (oldAttrs: {
+              doCheck = false;
+            })
+          else
+            x
+        ) perlDeps.paths;
+      };
+      patchInputs = inputs: map (x: if x.name == "hydra-perl-deps" then newPerlDeps else x) inputs;
+    in
+    {
+      nativeBuildInputs = patchInputs oldAttrs.nativeBuildInputs;
+      buildInputs = patchInputs oldAttrs.buildInputs;
+      passthru.perlDeps = newPerlDeps;
+    }
+  );
+
   forgejo = latest.forgejo.overrideAttrs {
     src = inputs.forgejo;
     version = versionFromInput inputs.forgejo;
